a4d1281bff3f8c499564ec86902e8223d0715dc5
Kate Drogaieva <kdrogaieva@lerning.com>
2025-04-30 16:21:18 -0700

    Add "sku" source for Fivetran

--- dbt/LCom_DW/models/reporting/get_monthly_snapshot_data_district_level.sql
+++ dbt/LCom_DW/models/reporting/get_monthly_snapshot_data_district_level.sql
@@ -22,7 +22,7 @@ from {{ ref("fact_license_order") }} flo
 join {{ ref("dim_district") }} dist
 on flo.organization_district_id = dist.district_id
 --
-join {{ ref("sku") }} s
+join {{ source("common","sku") }} s
 on flo.sku_id = s.skuid
 --
 join {{ ref("dim_license_order_school") }} dlos
