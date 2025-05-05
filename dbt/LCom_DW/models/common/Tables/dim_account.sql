8ca78f382b22ad259d0600ec3a6fe295eb781cc4
Kate Drogaieva <kdrogaieva@lerning.com>
2025-05-05 14:46:40 -0700

    FK created at the start of a project and ongoing recreation

--- dbt/LCom_DW/models/common/Tables/dim_account.sql
+++ dbt/LCom_DW/models/common/Tables/dim_account.sql
@@ -3,9 +3,23 @@
 
         materialized='table',        
         dist='account_id', 
-        sort='account_id'     
+        sort='account_id',
+        post_hook=['{{ create_FK(target.database,"licensing","dim_license_order_school","organization_school_id",model.schema,model.name, "account_id") }}',
+                   '{{ create_FK(target.database,"licensing","dim_license_order_school","organization_district_id",model.schema,model.name, "account_id") }}',
+                   '{{ create_FK(target.database,"licensing","fact_license_order","organization_district_id",model.schema,model.name, "account_id") }}',
+                   '{{ create_FK(target.database,"licensing","fact_license_order_history","organization_district_id",model.schema,model.name, "account_id") }}',                   
+                   '{{ create_FK(target.database,"revenue","fact_opportunity","account_id",model.schema,model.name, "account_id") }}',
+                   '{{ create_FK(target.database,"revenue","fact_opportunity_history","account_id",model.schema,model.name, "account_id") }}',    
+                   '{{ create_FK(target.database,"content_delivery_usage","fact_launches_monthly_snapshots","organization_district_id",model.schema,model.name, "account_id") }}',
+                   '{{ create_FK(target.database,"content_delivery_usage","fact_launches_weekly_snapshots","organization_district_id",model.schema,model.name, "account_id") }}',                                  
+                   '{{ create_FK(target.database,"content_delivery_usage","fact_launches_monthly_snapshots","organization_school_id",model.schema,model.name, "account_id") }}',
+                   '{{ create_FK(target.database,"content_delivery_usage","fact_launches_weekly_snapshots","organization_school_id",model.schema,model.name, "account_id") }}',                                  
+                    
+                    
+        ]
         )
 }}
+
 
 
 with sch as (
