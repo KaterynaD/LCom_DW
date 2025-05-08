793d5571281dcb3d64bf8e23727d91b71eb40215
Kate Drogaieva <kdrogaieva@lerning.com>
2025-05-08 16:06:05 -0700

    Deadlock issue in Fivetran

--- dbt/LCom_DW/models/content_delivery_usage/Tables/Dims/dim_lcom_sku_learning_object.sql
+++ dbt/LCom_DW/models/content_delivery_usage/Tables/Dims/dim_lcom_sku_learning_object.sql
@@ -13,10 +13,10 @@ select
 lower(stg.skuid) as sku_id,
 lower(stg.CurriculumItemId) as learning_object_id
 from {{ source("staging", "sku_learning_object_v2") }} stg
-join {{ ref("dim_lcom_sku")}} sku
+/*join {{ ref("dim_lcom_sku")}} sku
     on lower(stg.skuid) = sku.sku_id
 join {{ ref("dim_learning_object")}} lo
-    on lower(stg.CurriculumItemId) = lo.learning_object_id
+    on lower(stg.CurriculumItemId) = lo.learning_object_id*/
 where stg.skuid is not null and stg.CurriculumItemId is not null
 )
 select
