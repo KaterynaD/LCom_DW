e032faa1bd1b2c79b12b5ef1e318a6a7a848cfd1
Kate Drogaieva <kdrogaieva@lerning.com>
2025-05-08 13:45:09 -0700

    New SKU,Suite and Product related tables to vacuum and analyze

--- dbt/LCom_DW/macros/utils/run_vacuum_and_analyze.sql
+++ dbt/LCom_DW/macros/utils/run_vacuum_and_analyze.sql
@@ -11,14 +11,21 @@ vacuum {{ ref('fact_license_order') }};
 vacuum {{ ref('fact_license_order_history') }};
 --
 vacuum {{ ref('fact_opportunity_history') }};
-
+--
+vacuum {{ ref('dim_license_order_school) }};
+vacuum {{ ref('dim_lcom_sku) }};
+vacuum {{ ref('dim_sfdc_product) }};
+--
 
 analyze {{ ref('fact_license_order') }};
 analyze {{ ref('fact_license_order_history') }};
 --
 analyze {{ ref('fact_opportunity_history') }};
  
-
+--
+analyze {{ ref('dim_license_order_school') }};
+analyze {{ ref('dim_lcom_sku') }};
+analyze {{ ref('dim_sfdc_product') }};
 
  {% endset %}
 
