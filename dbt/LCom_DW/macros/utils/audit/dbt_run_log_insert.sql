79e308f2ed2147beeafb9336bbe3190c43ecf4e7
Kate Drogaieva <kdrogaieva@learning.com>
2025-05-24 18:06:01 -0700

    CHanging audit mode from insert/update to insert Start/End

--- dbt/LCom_DW/macros/utils/audit/dbt_run_log_insert.sql
+++ dbt/LCom_DW/macros/utils/audit/dbt_run_log_insert.sql
@@ -4,7 +4,7 @@
     
 
 
- {% set insert_starttime_operation %}
+ {% set insert_time_operation %}
 
  {% set run_at_date = get_datetime_now() %}
 
@@ -18,12 +18,24 @@
 
 {% endif %}
  
- INSERT INTO {{ source("audit","dbt_run_log") }} (run_id, start_time, operation, comments) VALUES ('{{ invocation_id }}','{{ run_at_date }}', '{{ run_operation }}','{{ comments }}');
+
+{% if comments|length > 1  %} 
+  
+  {% set run_comments =  comments  %}
+
+{% else %} 
+ 
+  {% set run_comments =   var('run_type')   %}
+
+{% endif %}
+
+
+ INSERT INTO {{ source("audit","dbt_run_log") }} (run_id, operation_time, operation, comments) VALUES ('{{ invocation_id }}','{{ run_at_date }}', '{{ run_operation }}','{{ run_comments }}');
 
 
  {% endset %}
 
-{% do run_query(insert_starttime_operation) %}
+{% do run_query(insert_time_operation) %}
 
  {%- endif %}
 
