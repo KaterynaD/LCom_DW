97d2dc60455f15fccaebe4f1fe825b3c5b3dfa55
Kate Drogaieva <kdrogaieva@learning.com>
2025-05-29 11:09:21 -0700

    Adjusting SKU_Groups (Tech Apps for Texas) and columns renaming

--- dbt/LCom_DW/models/common/Tables/dim_lcom_sku.sql
+++ dbt/LCom_DW/models/common/Tables/dim_lcom_sku.sql
@@ -1,11 +1,12 @@
 {{ config(
         
-        materialized='incremental',
-        unique_key=['sku_id'],
-        incremental_strategy='merge',
-        on_schema_change='append_new_columns',
+        materialized='table',
         dist='all',
-        sort='sku_name'
+        sort='sku_name',
+        post_hook=[
+                '{{ create_FK(target.database,"licensing","fact_license_order","sku_id",model.schema,model.name, "sku_id") }}',
+                '{{ create_FK(target.database,"licensing","fact_license_order_history","sku_id",model.schema,model.name, "sku_id") }}'
+                  ]
 )
  }}
 
@@ -14,14 +15,15 @@ select
 lower(sku.skuid) sku_id,
 case when sku.skuname ilike '%easy%tech%' then 'EasyTech'
      when sku.skuname ilike '%easy%code%' then 'EasyCode'     
-     else isnull(sku.ProductName,'{{ var("default_varchar") }}')
-end as product_line,
+     when sku.skuname ilike '%techapps%for%texas%' then  'Tech Apps for Texas'   
+     else isnull(sku.ProductName,'Other')
+end as sku_group,
 case 
 when sku.skuname ilike '%online%safety%' then 'Online Safety'
 when sku.skuname ilike '%keyboarding%' then 'Keyboarding'
 when sku.skuname ilike '%easy%code%pillars%' then 'EasyCode Pillars'
 when sku.skuname ilike '%easy%code%foundations%' then 'EasyCode Foundations'
-end as product_name,
+end as sku_subgroup,
 isnull(sku.skuname,'{{ var("default_varchar") }}') as sku_name,
 isnull(sku.description,'{{ var("default_varchar") }}') as sku_description,
 isnull(sku.parentskuid,'{{ var("default_varchar") }}') as parent_sku_id,
@@ -40,8 +42,8 @@ where coalesce(sku.auditupdatedate,'1900-01-01') >= (select coalesce(max(t.audit
 union all
 select
 '{{ var("default_ID") }}' sku_id,
-'{{ var("default_varchar") }}' as product_line,
-'{{ var("default_varchar") }}' as product_name,
+'{{ var("default_varchar") }}' as sku_group,
+'{{ var("default_varchar") }}' as sku_subgroup,
 '{{ var("default_varchar") }}' as sku_name,
 '{{ var("default_varchar") }}' as sku_description,
 '{{ var("default_varchar") }}' as parent_sku_id,
@@ -54,8 +56,8 @@ select
 )
 select
  sku_id::VARCHAR(50) as sku_id
-,product_line::VARCHAR(200) as product_line
-,isnull(product_name,product_line)::VARCHAR(200) as product_name
+,sku_group::VARCHAR(200) as sku_group
+,isnull(sku_subgroup,sku_group)::VARCHAR(200) as sku_subgroup
 ,sku_name::VARCHAR(150) as sku_name
 ,sku_description::VARCHAR(1100) as sku_description
 ,parent_sku_id::VARCHAR(100) as parent_sku_id
