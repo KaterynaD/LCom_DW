e6b2f3b0cf9bcd8d3c9348a2503e066932f013b5
Kate Drogaieva <kdrogaieva@learning.com>
2025-06-03 09:51:32 -0700

    Fix - including nulls in stg.NetSuiteOrderId

--- dbt/LCom_DW/models/licensing/Tables/Dims/dim_license_order_school.sql
+++ dbt/LCom_DW/models/licensing/Tables/Dims/dim_license_order_school.sql
@@ -21,12 +21,12 @@ join {{ source("staging","license_orderschool") }}  ord_sch
 on stg.orderid=ord_sch.orderid
 --
 left outer join {{ ref("dim_account") }} dist
-on lower(stg.ownerid) = dist.lcom_organization_id
+on lower(stg.ownerid) = dist.account_id
 --
 left outer join {{ ref("dim_account") }} sch
-on lower(ord_sch.schoolid) = sch.lcom_organization_id
+on lower(ord_sch.schoolid) = sch.account_id
 --
 where ord_sch.Valid_boolean=true
 {% if is_incremental() %}
- and coalesce(stg.auditupdatedate,'1900-01-01') >= (select coalesce(max(t.auditupdatedate),'1900-01-01') from {{ ref("stg_license_orders") }}  t)
+ and coalesce(stg.auditupdatedate,'1900-01-01') >= (select coalesce(max(t.auditupdatedate),'1900-01-01') from {{ this }} t )
 {% endif %}
--- dbt/LCom_DW/models/staging/Views/stg_license_orders.sql
+++ dbt/LCom_DW/models/staging/Views/stg_license_orders.sql
@@ -6,7 +6,7 @@
 select * from {{ source("staging","license_order") }} stg
 where
 stg.Valid_boolean=true and
-stg.NetSuiteOrderId != 'COVID19' and
+isnull(stg.NetSuiteOrderId,'~') != 'COVID19' and
 stg.SkuId NOT IN ('63CC161C-A834-4017-8281-ED0C4C4C52B5' --Google Integration
                  ,'CA3A733E-0F33-4918-897B-506C6BD6907C' --Generic LTI Tool Consumer
                  ,'545DA6FB-B65D-4EA8-A374-30BEEAAA28D4' --Tech Apps TCEA Assessment MS 19/20+'
