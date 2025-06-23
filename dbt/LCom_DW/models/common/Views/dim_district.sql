cce0e9eb0683dbf473d6b2ca5ff7c6d37399bcb8
Kate Drogaieva <kdrogaieva@learning.com>
2025-06-23 15:53:02 -0700

    District Enrollment column added

--- dbt/LCom_DW/models/common/Views/dim_district.sql
+++ dbt/LCom_DW/models/common/Views/dim_district.sql
@@ -58,6 +58,7 @@ SFDC_parent_gsid	,
 SFDC_technology_equipment	,
 SFDC_technology_supplies_and_purchases	,
 SFDC_type	,
+SFDC_district_enrollment,
 --Other useful SFDC columns
 sfdc_state_program_eligible ,
 SFDC_district_state_initiative	,
