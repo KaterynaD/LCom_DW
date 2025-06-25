76adf57e300a295d694b7ed67941b3d6f34d91c8
Kate Drogaieva <kdrogaieva@learning.com>
2025-06-24 19:00:10 -0700

    SUM by district and SKU, MAX by district and SUM by Company

--- dbt/LCom_DW/models/reporting/vw_licensing_scorecard.sql
+++ dbt/LCom_DW/models/reporting/vw_licensing_scorecard.sql
@@ -1,9 +1,11 @@
 {{ config(materialized='view', bind=False) }}
 
-with data as (select
+with 
+rawdata as (select
 a.account_id,
-max(schoolcount) max_schools,
-max(studentcount) max_students
+flo.sku_id,
+sum(schoolcount) sum_schools,
+sum(studentcount) sum_students
 from {{ ref("fact_license_order") }} flo 
 join {{ ref("dim_account") }} a
 on flo.organization_district_id = a.account_id
@@ -11,7 +13,15 @@ where ( GETDATE() between startdate and expirationdate
 or enforcedaterestrictions = 'n')
 and a.lcom_trial=false 
 and a.lcom_demo=false
-group by a.account_id
+group by a.account_id,
+flo.sku_id 
+)
+, data as (select
+account_id,
+max(sum_schools) max_schools,
+max(sum_students) max_students
+from rawdata
+group by account_id
 )
 select 
 count(distinct account_id) cnt_districts,
