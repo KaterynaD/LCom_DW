513cacbd43a179ff7ab7c5e814abf0bc4499048c
Kate Drogaieva <kdrogaieva@learning.com>
2025-06-24 18:40:26 -0700

    Max studentcount and schoolcount per district then sum

--- dbt/LCom_DW/models/reporting/vw_licensing_scorecard.sql
+++ dbt/LCom_DW/models/reporting/vw_licensing_scorecard.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='view', bind=False) }}
 
-select
-count(distinct organization_district_id) cnt_districts,
-sum(schoolcount) cnt_schools,
-sum(studentcount) cnt_students
+with data as (select
+a.account_id,
+max(schoolcount) max_schools,
+max(studentcount) max_students
 from {{ ref("fact_license_order") }} flo 
 join {{ ref("dim_account") }} a
 on flo.organization_district_id = a.account_id
@@ -11,3 +11,10 @@ where ( GETDATE() between startdate and expirationdate
 or enforcedaterestrictions = 'n')
 and a.lcom_trial=false 
 and a.lcom_demo=false
+group by a.account_id
+)
+select 
+count(distinct account_id) cnt_districts,
+sum(max_schools) cnt_school,
+sum(max_students) cnt_students
+from data
