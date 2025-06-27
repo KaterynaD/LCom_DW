b393dd7975ab99a6a3c36e55852506213e27b04c
Kate Drogaieva <kdrogaieva@learning.com>
2025-06-27 08:40:08 -0700

    Actual, Target and Previous categories and Last_Update in all scorecard views

--- dbt/LCom_DW/models/reporting/vw_customer_scorecard.sql
+++ dbt/LCom_DW/models/reporting/vw_customer_scorecard.sql
@@ -7,12 +7,26 @@ with dim_month as
  (
 select distinct FiscalYear, FiscalYear_StartDate, FiscalYear_EndDate, FiscalYear_Mon, Mon_FirstDay, Mon_LastDay,Mon_Year
 from {{ source("common","dim_calendar") }}
-where TIMEZONE('UTC', GetDate()) between Mon_FirstDay and Mon_LastDay
+where GetDate() between Mon_FirstDay and Mon_LastDay
+)
+,dim_month_prev as
+ (
+select distinct FiscalYear, FiscalYear_StartDate, FiscalYear_EndDate, FiscalYear_Mon, Mon_FirstDay, Mon_LastDay,Mon_Year
+from {{ source("common","dim_calendar") }}
+where date_add('year', -1, GetDate()) between Mon_FirstDay and Mon_LastDay
 )
 ,contract_based_active_rawdata as (
 select sfdc_ultimate_parent_id
 from {{ ref("fact_customers_monthly_snapshots") }} s
 join dim_month m
+on s.Mon_LastDay = last_day(dateadd('month',1, m.Mon_LastDay))
+where include_flg=true
+and record_type='Starting'
+)
+,contract_based_active_rawdata_prev as (
+select sfdc_ultimate_parent_id
+from {{ ref("fact_customers_monthly_snapshots") }} s
+join dim_month_prev m
 on s.Mon_LastDay = last_day(dateadd('month',1, m.Mon_LastDay))
 where include_flg=true
 and record_type='Starting'
@@ -46,6 +60,35 @@ on s.mon_lastday between m.FiscalYear_StartDate and m.FiscalYear_EndDate
 where include_flg=true
 and record_type in ('NonRenewal','Ghost' )
 )
+, net_active_rawdata_prev as (
+select sfdc_ultimate_parent_id
+from {{ ref("fact_customers_monthly_snapshots") }} s
+join dim_month_prev m
+on s.mon_year = to_char(m.FiscalYear_StartDate,'yyyymm') 
+where include_flg=true
+and record_type='Starting'
+union
+select sfdc_ultimate_parent_id
+from {{ ref("fact_customers_monthly_snapshots") }} s
+join dim_month_prev m
+on s.mon_lastday between m.FiscalYear_StartDate and m.FiscalYear_EndDate
+where include_flg=true
+and record_type in ('New','Returning','Returning','ReturningFY' )
+except
+select sfdc_ultimate_parent_id
+from {{ ref("fact_customers_monthly_snapshots") }} s
+join dim_month_prev m
+on s.mon_lastday between m.FiscalYear_StartDate and m.FiscalYear_EndDate
+where include_flg=true
+and record_type in ('Churn' )
+except
+select sfdc_ultimate_parent_id
+from {{ ref("fact_customers_monthly_snapshots") }} s
+join dim_month_prev m
+on s.mon_lastday between m.FiscalYear_StartDate and m.FiscalYear_EndDate
+where include_flg=true
+and record_type in ('NonRenewal','Ghost' )
+)
 ,total_active_rawdata as (
 select sfdc_ultimate_parent_id
 from {{ ref("fact_customers_monthly_snapshots") }} s
@@ -61,12 +104,34 @@ on s.mon_lastday between m.FiscalYear_StartDate and m.FiscalYear_EndDate
 where include_flg=true
 and record_type in ('New','Returning')
 )
+,total_active_rawdata_prev as (
+select sfdc_ultimate_parent_id
+from {{ ref("fact_customers_monthly_snapshots") }} s
+join dim_month_prev m
+on s.mon_year = to_char(m.FiscalYear_StartDate,'yyyymm') 
+where include_flg=true
+and record_type='Starting'
+union
+select sfdc_ultimate_parent_id
+from {{ ref("fact_customers_monthly_snapshots") }} s
+join dim_month_prev m
+on s.mon_lastday between m.FiscalYear_StartDate and m.FiscalYear_EndDate
+where include_flg=true
+and record_type in ('New','Returning')
+)
 ,net_active_data as (
 select dim_month.FiscalYear, count(distinct sfdc_ultimate_parent_id) cnt_customers
 from net_active_rawdata
 join dim_month
 on 1=1
 group by dim_month.FiscalYear
+)
+,net_active_data_prev as (
+select dim_month_prev.FiscalYear, count(distinct sfdc_ultimate_parent_id) cnt_customers
+from net_active_rawdata_prev
+join dim_month_prev
+on 1=1
+group by dim_month_prev.FiscalYear
 )
 ,total_active_data as (
 select dim_month.FiscalYear, count(distinct sfdc_ultimate_parent_id) cnt_customers
@@ -75,6 +140,13 @@ join dim_month
 on 1=1
 group by dim_month.FiscalYear
 )
+,total_active_data_prev as (
+select dim_month_prev.FiscalYear, count(distinct sfdc_ultimate_parent_id) cnt_customers
+from total_active_rawdata_prev
+join dim_month_prev
+on 1=1
+group by dim_month_prev.FiscalYear
+)
 ,contract_based_active_data as (
 select dim_month.FiscalYear, count(distinct sfdc_ultimate_parent_id) cnt_customers
 from contract_based_active_rawdata
@@ -82,13 +154,69 @@ join dim_month
 on 1=1
 group by dim_month.FiscalYear
 )
+,contract_based_active_data_prev as (
+select dim_month_prev.FiscalYear, count(distinct sfdc_ultimate_parent_id) cnt_customers
+from contract_based_active_rawdata_prev
+join dim_month_prev
+on 1=1
+group by dim_month_prev.FiscalYear
+)
+,datedata as (select least(max(created_date), max(last_modified_date), max(mon_lastday), max(fcms.loaddate)) last_updated from {{ ref("fact_opportunity") }} fo join {{ ref("fact_customers_monthly_snapshots") }} fcms on 1=1)
+,vw_customer_scorecard as (
 select 
-net_active_data.FiscalYear, 
+dim_month.FiscalYear,
 contract_based_active_data.cnt_customers contract_based_active,
 net_active_data.cnt_customers net_active, 
-total_active_data.cnt_customers total_active
+total_active_data.cnt_customers total_active,
+datedata.last_updated
 from net_active_data
 join total_active_data
 on 1=1
 join contract_based_active_data
 on 1=1
+join datedata
+on 1=1
+join dim_month
+on 1=1
+)
+,vw_customer_scorecard_prev as (select 
+dim_month_prev.FiscalYear,
+contract_based_active_data_prev.cnt_customers contract_based_active,
+net_active_data_prev.cnt_customers net_active, 
+total_active_data_prev.cnt_customers total_active,
+Mon_LastDay last_updated
+from net_active_data_prev
+join total_active_data_prev
+on 1=1
+join contract_based_active_data_prev
+on 1=1
+join dim_month_prev
+on 1=1
+)
+select 
+'Actual' as category,
+contract_based_active,
+net_active, 
+total_active,
+FiscalYear,
+last_updated
+from vw_customer_scorecard
+union all
+select 
+'Previous' as category,
+contract_based_active,
+net_active, 
+total_active,
+FiscalYear,
+last_updated
+from vw_customer_scorecard_prev
+union all
+select 
+'Target' as category,
+0 contract_based_active,
+0 net_active, 
+0 total_active,
+'N/A' FiscalYear,
+cast('1900-01-01' as date) last_updated
+
+
--- dbt/LCom_DW/models/reporting/vw_licensing_scorecard.sql
+++ dbt/LCom_DW/models/reporting/vw_licensing_scorecard.sql
@@ -1,7 +1,18 @@
 {{ config(materialized='view', bind=False) }}
 
+
 with 
-rawdata as (select
+dim_date as (
+select distinct SchoolYear, SchoolYear_StartDate, SchoolYear_EndDate, SchoolYear_Mon, Mon_FirstDay, Mon_LastDay,Mon_Year
+from {{ source("common","dim_calendar") }}
+where GetDate() between Mon_FirstDay and Mon_LastDay
+)
+,dim_date_prev as (
+select distinct SchoolYear, SchoolYear_StartDate, SchoolYear_EndDate, SchoolYear_Mon, Mon_FirstDay, Mon_LastDay,Mon_Year
+from {{ source("common","dim_calendar") }}
+where  date_add('year', -1, GetDate()) between Mon_FirstDay and Mon_LastDay
+)
+,rawdata as (select
 a.account_id,
 flo.sku_id,
 sum(schoolcount) sum_schools,
@@ -23,8 +34,47 @@ max(sum_students) max_students
 from rawdata
 group by account_id
 )
+,datedata as (select greatest(max(auditcreatedate), max(auditupdatedate)) last_updated from {{ ref("fact_license_order") }} flo)
+,vw_licensing_scorecard as(
 select 
 count(distinct account_id) cnt_districts,
-sum(max_schools) cnt_school,
-sum(max_students) cnt_students
+sum(max_schools) cnt_schools,
+sum(max_students) cnt_students,
+dim_date.SchoolYear,
+datedata.last_updated
 from data
+join datedata
+on 1=1
+join dim_date
+on 1=1
+group by last_updated,dim_date.SchoolYear
+)
+select 
+'Actual' as category,
+cnt_districts,
+cnt_schools,
+cnt_students,
+SchoolYear,
+last_updated
+from vw_licensing_scorecard
+union all
+select 
+'Previous' as category, 
+count(distinct organization_district_id) as cnt_districts, 
+sum(schoolcount) as cnt_school, 
+sum(studentcount) as cnt_students,
+dim_date_prev.SchoolYear,
+vlm.mon_lastday last_updated
+from {{ ref("vw_licensing_monthly") }} vlm 
+join dim_date_prev
+on  vlm.mon_year = dim_date_prev.Mon_Year
+where sku_name='(All)'
+group by vlm.mon_lastday,dim_date_prev.SchoolYear
+union all
+select 
+'Target' as category,
+0 as cnt_districts, 
+0 as cnt_school, 
+0 as cnt_students,
+'N/A' FiscalYear,
+cast('1900-01-01' as date) last_updated
