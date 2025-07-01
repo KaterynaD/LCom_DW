289a49b062bbd2f9ada83236b923e9e189ade81d
Kate Drogaieva <kdrogaieva@learning.com>
2025-07-01 15:26:13 -0700

    fix in deleting garbage cases

--- dbt/LCom_DW/macros/support_models/fact_case_history/update_FACT_CASE_HISTORY_changed_UK.sql
+++ dbt/LCom_DW/macros/support_models/fact_case_history/update_FACT_CASE_HISTORY_changed_UK.sql
@@ -25,7 +25,7 @@ where data.sfdc_account_id=support.fact_case_history.account_id;
 /*Some cases are deleted in SFDC*/
 /*and not included in FACT_CASE next day               */
 delete from support.fact_case_history 
-where case_id not in (select case_id from support.fact_case_history);
+where case_id not in (select case_id from support.fact_case);
 
  {% endset %}
 
