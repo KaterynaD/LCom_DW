b3bba6b291f6c8bef3e832a5745c1be10fa49064
Kate Drogaieva <kdrogaieva@learning.com>
2025-07-01 09:03:39 -0700

    deadlock

--- dbt/LCom_DW/models/common/Tables/dim_lcom_suite_sku.sql
+++ dbt/LCom_DW/models/common/Tables/dim_lcom_suite_sku.sql
@@ -16,6 +16,11 @@ with data as
     lower(stg.lcom_suite_id) as suite_id,
     lower(stg.skuid) as sku_id
 from {{ source("staging","sku_suite") }} stg
+/*join {{ ref("dim_lcom_sku")}} sku
+    on lower(stg.skuid) = sku.sku_id
+join {{ ref("dim_lcom_suite")}} suite
+    on lower(stg.lcom_suite_id) = suite.suite_id
+    */
 where stg.lcom_suite_id is not null and stg.skuid is not null
 )
 select
