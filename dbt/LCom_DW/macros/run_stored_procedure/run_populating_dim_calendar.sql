940d52f70d3b576210a478aad0148733746a622f
Kate Drogaieva <kdrogaieva@learning.com>
2025-07-24 13:33:42 -0700

    Restoring proper Prod version of the stored procedure

--- dbt/LCom_DW/macros/run_stored_procedure/run_populating_dim_calendar.sql
+++ dbt/LCom_DW/macros/run_stored_procedure/run_populating_dim_calendar.sql
@@ -7,7 +7,7 @@
  {% set run_sp_operation %}
 
  
- call {{target.database}}.common.populating_dim_calendar(cast('2015-07-01' as date), cast('2035-06-30' as date));
+ call {{target.database}}.common.populating_dim_calendar(cast('2015-08-01' as date), cast('2035-06-30' as date));
  
 
 
