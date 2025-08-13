245d7d0e1faaa904b4d99869e39a1f0706b3144a
Kate Drogaieva <kdrogaieva@learning.com>
2025-08-13 11:55:45 -0700

    Fix: direct table to ref

--- dbt/LCom_DW/tests/content_delivery_usage/fact_students_usage_monthly_snapshots/all_current.sql
+++ dbt/LCom_DW/tests/content_delivery_usage/fact_students_usage_monthly_snapshots/all_current.sql
@@ -1,4 +1,4 @@
-select schoolyear,unique_students, unique_students_launches from reporting.vw_usage_scorecard vus where category='Actual'
+select schoolyear,unique_students, unique_students_launches from {{ ref("vw_usage_scorecard") }} vus where category='Actual'
 except
 select distinct schoolyear, company_cnt_students , company_students_launches 
 from {{ ref("fact_students_usage_monthly_snapshots") }}
