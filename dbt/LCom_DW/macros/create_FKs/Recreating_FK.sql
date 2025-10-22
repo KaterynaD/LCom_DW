96fd992f316ac584ac149a9ba10bd0ce32556246
Kate Drogaieva <kdrogaieva@learning.com>
2025-10-22 08:10:54 -0700

    dim_topic to dim_training_session_topic renaming

--- dbt/LCom_DW/macros/create_FKs/Recreating_FK.sql
+++ dbt/LCom_DW/macros/create_FKs/Recreating_FK.sql
@@ -58,10 +58,10 @@ ALTER TABLE content_delivery_usage.fact_training_session_history ADD FOREIGN KEY
 ALTER TABLE revenue.dim_opportunity_line ADD FOREIGN KEY (sfdc_product_id) REFERENCES common.dim_sfdc_product(sfdc_product_id);
 
 -- TO content_delivery_usage.dim_topic
-ALTER TABLE content_delivery_usage.dim_training_session_topic ADD FOREIGN KEY (topic_id) REFERENCES content_delivery_usage.dim_topic(topic_id);
+ALTER TABLE content_delivery_usage.dim_training_session_topic_session ADD FOREIGN KEY (topic_id) REFERENCES content_delivery_usage.dim_training_session_topic(topic_id);
 
 -- TO content_delivery_usage.fact_training_session
-ALTER TABLE content_delivery_usage.dim_training_session_topic ADD FOREIGN KEY (training_session_id) REFERENCES content_delivery_usage.fact_training_session(training_session_id);
+ALTER TABLE content_delivery_usage.dim_training_session_topic_session ADD FOREIGN KEY (training_session_id) REFERENCES content_delivery_usage.fact_training_session(training_session_id);
 
 
 
