50f7787739dc2556bc15232c22cbc784f4e43c65
Kate Drogaieva <kdrogaieva@learning.com>
2025-12-05 09:42:23 -0800

    FK droping and recreating before and after transformation run

---
+++ dbt/LCom_DW/macros/create_FKs/Droping_all_FK.sql
@@ -0,0 +1,26 @@
+{% macro Droping_all_FK()  %}
+
+{% set fks %}
+
+SELECT  
+'ALTER TABLE '+ constraint_schema + '.'+ table_name +' DROP CONSTRAINT '+constraint_name+';' drop_FK_SQL
+FROM information_schema.table_constraints
+WHERE constraint_type = 'FOREIGN KEY'
+and constraint_schema in ('licensing','revenue','content_delivery_usage','support','common')
+ORDER BY constraint_schema, table_name;
+
+{% endset %}
+
+{% set fks_to_drop = run_query(fks) %}
+
+{% if fks_to_drop | length == 0 %}
+      {{ log('⚠️ No FKs found to drop', info=True) }}
+{% else %}
+     {% for row in fks_to_drop.rows %}
+        {% set drop_FK_SQL = row[0] %}
+        {{ log('Dropping FK: ' ~ drop_FK_SQL, info=True) }}
+        {% do run_query(drop_FK_SQL) %}
+    {% endfor %}
+{% endif %}
+
+{% endmacro  %}
--- dbt/LCom_DW/macros/create_FKs/Recreating_all_FK.sql
+++ dbt/LCom_DW/macros/create_FKs/Recreating_all_FK.sql
@@ -1,7 +1,8 @@
-{% macro Recreating_FK()  %}
+{% macro Recreating_all_FK()  %}
 
 
 {% set create_FKs_operation %}
+
 
 
 -- TO common.dim_account
@@ -21,11 +22,15 @@ ALTER TABLE content_delivery_usage.fact_launches_weekly_snapshots ADD FOREIGN KE
 ALTER TABLE content_delivery_usage.fact_students_usage_monthly_snapshots ADD FOREIGN KEY (organization_district_id) REFERENCES common.dim_account(account_id);   
 ALTER TABLE content_delivery_usage.fact_students_usage_monthly_snapshots ADD FOREIGN KEY (organization_school_id) REFERENCES common.dim_account(account_id);
 --
+ALTER TABLE content_delivery_usage.fact_students_completions_monthly_snapshots ADD FOREIGN KEY (organization_district_id) REFERENCES common.dim_account(account_id);   
+ALTER TABLE content_delivery_usage.fact_students_completions_monthly_snapshots ADD FOREIGN KEY (organization_school_id) REFERENCES common.dim_account(account_id);
+--
 ALTER TABLE support.fact_case ADD FOREIGN KEY (account_id) REFERENCES common.dim_account(account_id);
 ALTER TABLE support.fact_case_history ADD FOREIGN KEY (account_id) REFERENCES common.dim_account(account_id);
 --
 ALTER TABLE content_delivery_usage.fact_training_session ADD FOREIGN KEY (account_id) REFERENCES common.dim_account(account_id);
 ALTER TABLE content_delivery_usage.fact_training_session_history ADD FOREIGN KEY (account_id) REFERENCES common.dim_account(account_id);
+
 -- TO common.dim_employee
 ALTER TABLE support.fact_case ADD FOREIGN KEY (owner_id) REFERENCES common.dim_employee(employee_id);
 ALTER TABLE support.fact_case_history ADD FOREIGN KEY (owner_id) REFERENCES common.dim_employee(employee_id);
@@ -38,6 +43,11 @@ ALTER TABLE licensing.fact_license_order ADD FOREIGN KEY (sku_id) REFERENCES com
 ALTER TABLE licensing.fact_license_order_history ADD FOREIGN KEY (sku_id) REFERENCES common.dim_lcom_sku(sku_id);
 ALTER TABLE content_delivery_usage.dim_lcom_sku_learning_object ADD FOREIGN KEY (sku_id) REFERENCES common.dim_lcom_sku(sku_id);
 ALTER TABLE common.dim_lcom_suite_sku ADD FOREIGN KEY (sku_id) REFERENCES common.dim_lcom_sku(sku_id);
+
+-- TO content_delivery_usage.dim_learning_object
+ALTER TABLE content_delivery_usage.dim_lcom_sku_learning_object ADD FOREIGN KEY (learning_object_id) REFERENCES content_delivery_usage.dim_learning_object(learning_object_id);
+ALTER TABLE content_delivery_usage.dim_sequence_learning_object ADD FOREIGN KEY (learning_object_id) REFERENCES content_delivery_usage.dim_learning_object(learning_object_id);
+ALTER TABLE content_delivery_usage.dim_product_category_learning_object_monthly ADD FOREIGN KEY (learning_object_id) REFERENCES content_delivery_usage.dim_learning_object(learning_object_id);
 
 -- TO common.dim_lcom_suite
 ALTER TABLE common.dim_lcom_suite_sku ADD FOREIGN KEY (suite_id) REFERENCES common.dim_lcom_suite(suite_id);
@@ -56,6 +66,7 @@ ALTER TABLE content_delivery_usage.fact_training_session_history ADD FOREIGN KEY
 
 --TO common.dim_sfdc_product
 ALTER TABLE revenue.dim_opportunity_line ADD FOREIGN KEY (sfdc_product_id) REFERENCES common.dim_sfdc_product(sfdc_product_id);
+ALTER TABLE revenue.dim_opportunity_line_history ADD FOREIGN KEY (sfdc_product_id) REFERENCES common.dim_sfdc_product(sfdc_product_id);
 
 -- TO content_delivery_usage.dim_topic
 ALTER TABLE content_delivery_usage.dim_training_session_topic_session ADD FOREIGN KEY (topic_id) REFERENCES content_delivery_usage.dim_training_session_topic(topic_id);
@@ -63,6 +74,11 @@ ALTER TABLE content_delivery_usage.dim_training_session_topic_session ADD FOREIG
 -- TO content_delivery_usage.fact_training_session
 ALTER TABLE content_delivery_usage.dim_training_session_topic_session ADD FOREIGN KEY (training_session_id) REFERENCES content_delivery_usage.fact_training_session(training_session_id);
 
+--TO revenue.dim_opportunity_line
+ALTER TABLE revenue.dim_opportunity_line_history ADD FOREIGN KEY (opportunity_line_id) REFERENCES revenue.dim_opportunity_line(opportunity_line_id);
+
+--TO licensing.fact_license_order
+ALTER TABLE licensing.dim_license_order_school ADD FOREIGN KEY (order_id) REFERENCES licensing.fact_license_order(order_id);
 
 
 
