69ed11daae4a3a71bb55b4886c3e40870c72e32a
Kate Drogaieva <kdrogaieva@learning.com>
2025-12-11 13:28:36 -0800

    New views for licensing provisioning added

---
+++ dbt/LCom_DW/models/reporting/Views/vw_alive_accounts.sql
@@ -0,0 +1,170 @@
+{{ config(materialized='view', bind=False) }}
+
+
+with dim_month as --Thread to calculate monthly metrics
+(select substring(c.mon_year,5,2)::int mon, c.mon_year, c.mon_firstday, c.mon_lastday, c.FiscalYear, c.SchoolYear, c.FiscalYear_startdate, c.FiscalYear_enddate , c.FiscalYear_mon, c.SchoolYear_mon
+from {{ ref("dim_month") }} c
+where mon_year between 202207 and to_char(GetDate(),'yyyymm')
+)
+,alive_accounts as (
+select distinct account_id from {{ ref("fact_opportunity") }} 
+union
+select distinct organization_district_id from {{ ref("fact_license_order") }}
+union
+select distinct organization_school_id from {{ ref("dim_license_order_school") }}
+union
+select distinct organization_school_id from {{ ref("fact_students_usage_monthly_snapshots") }} 
+)
+--
+,data as (
+select
+m.mon,
+m.mon_year,
+m.mon_lastday ,
+m.FiscalYear,
+m.FiscalYear_mon,
+m.SchoolYear,
+m.SchoolYear_mon,
+--
+ah.lcom_organization_id as lcom_organization_id ,
+a.lcom_parent_organization_id as lcom_parent_organization_id ,
+--
+ah.sfdc_parent_id,
+ah.sfdc_ultimate_parent_id,
+--
+ah.account_id,
+ah.sfdc_account_id,
+ah.sfdc_name,
+ah.lcom_organization_name,
+case
+when ah.sfdc_name = 'Unknown'
+then ah.lcom_organization_name
+else ah.sfdc_name
+end as name,
+case when (ah.sfdc_state_initiative or ah.sfdc_state_initiative_school) then true else false end as state_initiative,
+case
+when ah.sfdc_billing_country = 'Unknown'
+then ah.lcom_country_name
+else ah.sfdc_billing_country
+end as country,
+case
+when ah.sfdc_billing_state_code = 'Unknown'
+then ah.lcom_state_province_code
+when ah.sfdc_billing_country = 'United States'
+then 'United States of America'
+else ah.sfdc_billing_state_code
+end as state,
+ah.sfdc_county_name as county,
+ah.sfdc_owner_name_text as owner,
+case
+when ah.sfdc_record_type = 'L' then ah.sfdc_district_enrollment
+else ah.sfdc_school_enrollment
+end as enrollment,
+case
+when ah.sfdc_record_type = 'L' then 'District'
+else 'School' END as account_type,
+ah.sfdc_urban_rural as urban_rural,
+--
+ah.LCOM_organization_type lcom_organization_type,
+--
+a.lcom_organization_id as current_lcom_organization_id ,
+a.lcom_parent_organization_id as current_lcom_parent_organization_id ,
+--
+a.sfdc_parent_id as current_sfdc_parent_id,
+a.sfdc_ultimate_parent_id as current_sfdc_ultimate_parent_id,
+--
+a.sfdc_name as current_sfdc_name,
+a.lcom_organization_name as current_lcom_organization_name ,
+case
+when a.sfdc_name = 'Unknown'
+then a.lcom_organization_name
+else a.sfdc_name
+end as current_name,
+case when (a.sfdc_state_initiative or a.sfdc_state_initiative_school) then true else false end as current_state_initiative,
+case
+when a.sfdc_billing_country = 'Unknown'
+then a.lcom_country_name
+when a.sfdc_billing_country = 'United States'
+then 'United States of America'
+else a.sfdc_billing_country
+end as current_country,
+case
+when a.sfdc_billing_state_code = 'Unknown'
+then a.lcom_state_province_code
+else a.sfdc_billing_state_code
+end as current_state,
+
+a.sfdc_county_name as current_county,
+a.sfdc_owner_name_text as current_owner,
+case
+when a.sfdc_record_type = 'L' then a.sfdc_district_enrollment
+else a.sfdc_school_enrollment
+end as current_enrollment,
+case
+when a.sfdc_record_type = 'L' then 'District'
+else 'School' END as current_account_type,
+a.sfdc_urban_rural as current_urban_rural,
+--
+a.LCOM_organization_type current_lcom_organization_type
+--
+from dim_month m
+join {{ ref("dim_account_history") }} ah
+on m.mon_lastday between ah.fromdate and ah.todate
+join alive_accounts la
+on la.account_id=ah.account_id
+join {{ ref("dim_account") }}  a
+on a.account_id = ah.account_id
+where a.lcom_trial=false
+and a.lcom_demo=false
+)
+select
+mon ,
+mon_year ,
+mon_lastday ,
+fiscalyear ,
+fiscalyear_mon ,
+schoolyear ,
+schoolyear_mon ,
+--
+account_id ,
+sfdc_account_id ,
+--
+lcom_organization_id ,
+lcom_parent_organization_id ,
+--
+sfdc_parent_id,
+sfdc_ultimate_parent_id,
+--
+sfdc_name ,
+lcom_organization_name ,
+name ,
+state_initiative ,
+country ,
+state ,
+county ,
+owner ,
+enrollment ,
+account_type ,
+urban_rural ,
+lcom_organization_type,
+--
+current_lcom_organization_id ,
+current_lcom_parent_organization_id ,
+--
+current_sfdc_parent_id,
+current_sfdc_ultimate_parent_id,
+--
+current_sfdc_name ,
+current_lcom_organization_name ,
+current_name ,
+current_state_initiative ,
+current_country ,
+current_state ,
+current_county ,
+current_owner ,
+current_enrollment ,
+current_account_type ,
+current_urban_rural ,
+current_lcom_organization_type
+from data
+
---
+++ dbt/LCom_DW/models/reporting/Views/vw_licenses_provioned_monthly.sql
@@ -0,0 +1,382 @@
+{{ config(materialized='view', bind=False) }}
+
+														
+with															
+cal as (															
+select															
+schoolyear,															
+substring(mon_year,5,2)::int mon,															
+mon_firstday,															
+mon_lastday,															
+schoolyear_mon															
+from															
+{{ ref("dim_month") }}														
+where mon_year between 202207 and to_char(GetDate(),'yyyymm')															
+)															
+,raw_license_data as (															
+select															
+mon_lastday,															
+mon,															
+schoolyear,															
+schoolyear_mon,															
+case when (dist.sfdc_state_initiative or dist.sfdc_state_initiative_school) then true else false end state_initiative,															
+dist.lcom_state_province_code district_state,															
+flo.order_id,															
+flo.organization_district_id,															
+flo.startdate,															
+flo.expirationdate,															
+flo.enforcedaterestrictions,															
+s.sku_name skuname,															
+flo.studentcount,															
+sch.ishighschool,															
+dlos.organization_school_id															
+from {{ ref("fact_license_order") }} flo															
+join {{ ref("dim_account") }} dist															
+on dist.account_id = flo.organization_district_id															
+-- 															
+join {{ ref("dim_lcom_sku") }} s															
+on flo.sku_id = s.sku_id															
+-- 															
+join {{ ref("dim_license_order_school") }} dlos															
+on flo.order_id = dlos.order_id															
+-- 															
+join {{ ref("dim_account") }} sch															
+on sch.account_id = dlos.organization_school_id															
+-- 															
+join cal on (															
+(															
+flo.StartDate <= cal.mon_lastday															
+and flo.ExpirationDate >= cal.mon_firstday															
+)															
+or (															
+flo.enforcedaterestrictions = 'n'															
+and flo.StartDate <= cal.mon_lastday															
+)															
+)															
+-- 															
+where dist.lcom_trial = false															
+and dist.lcom_demo= false																														
+)																													
+,rollup_schools_num as (															
+select															
+mon_lastday,															
+mon,															
+schoolyear,															
+schoolyear_mon,															
+state_initiative,															
+district_state,															
+organization_district_id,															
+startdate,															
+expirationdate,															
+enforcedaterestrictions,															
+skuname,															
+count(distinct case when ishighschool then organization_school_id end) as num_highschools,															
+count(distinct organization_school_id ) as num_schools															
+from raw_license_data															
+group by															
+mon_lastday,															
+mon,															
+schoolyear,															
+schoolyear_mon,															
+state_initiative,															
+district_state,															
+organization_district_id,															
+startdate,															
+expirationdate,															
+enforcedaterestrictions,															
+skuname															
+)															
+,rollup_studentcount as (															
+select															
+mon_lastday,															
+mon,															
+schoolyear,															
+schoolyear_mon,															
+state_initiative,															
+district_state,															
+organization_district_id,															
+startdate,															
+expirationdate,															
+enforcedaterestrictions,															
+skuname,															
+sum(studentcount) studentcount															
+from															
+(															
+select distinct															
+mon_lastday,															
+mon,															
+schoolyear,															
+schoolyear_mon,															
+state_initiative,															
+district_state,															
+organization_district_id,															
+startdate,															
+expirationdate,															
+enforcedaterestrictions,															
+skuname,															
+order_id,															
+studentcount															
+from raw_license_data															
+)															
+group by															
+mon_lastday,															
+mon,															
+schoolyear,															
+schoolyear_mon,															
+state_initiative,															
+district_state,															
+organization_district_id,															
+startdate,															
+expirationdate,															
+enforcedaterestrictions,															
+skuname															
+)															
+, t as (															
+select															
+sn.mon_lastday,															
+sn.mon,															
+sn.schoolyear,															
+sn.schoolyear_mon,															
+sn.state_initiative,															
+sn.district_state,															
+sn.organization_district_id,															
+sn.startdate,															
+sn.expirationdate,															
+sn.enforcedaterestrictions,															
+sn.skuname,															
+lead(sn.startdate) over (partition by sn.organization_district_id, sn.skuname, sn.mon_lastday order by sn.startdate) next_order,															
+case															
+when next_order is not null															
+and date_trunc('month', next_order) = date_trunc('month', sn.expirationdate)															
+and next_order > sn.expirationdate															
+and coalesce(sn.enforcedaterestrictions,'') <> 'n'															
+then 1															
+else 0															
+end as next_order_start_in_same_month,															
+sn.num_highschools,															
+sn.num_schools,															
+stc.studentcount															
+from rollup_schools_num sn															
+join rollup_studentcount stc															
+on															
+sn.mon_lastday = stc.mon_lastday															
+and sn.organization_district_id = stc.organization_district_id															
+and sn.startdate = stc.startdate															
+and sn.expirationdate = stc.expirationdate															
+and sn.enforcedaterestrictions = stc.enforcedaterestrictions															
+and sn.skuname = stc.skuname															
+)																													
+,district_sku_license_data as (															
+select															
+mon_lastday,															
+mon,															
+schoolyear,															
+schoolyear_mon,															
+state_initiative,															
+district_state,															
+organization_district_id,															
+skuname,															
+sum(															
+case															
+when next_order_start_in_same_month = 0 then studentcount															
+else 0															
+end															
+) as sum_studentcount,															
+sum(															
+case															
+when next_order_start_in_same_month = 0 and enforcedaterestrictions = 'y' then studentcount															
+else 0															
+end															
+) as sum_studentcount_restricted,															
+sum(															
+case															
+when next_order_start_in_same_month = 0															
+and district_state in ('NC','MI','SC','WV','GA')															
+and skuname ilike '%easy%tech%'															
+and num_highschools != num_schools then studentcount															
+when next_order_start_in_same_month = 0															
+and district_state in ('MS','FL')															
+and skuname ilike '%easy%tech%' then studentcount															
+else 0															
+end															
+) as state_initiative_studentcount,															
+sum(															
+case															
+when next_order_start_in_same_month = 0															
+and enforcedaterestrictions = 'y'															
+and district_state in ('NC','MI','SC','WV','GA')															
+and skuname ilike '%easy%tech%'															
+and num_highschools != num_schools then studentcount															
+when next_order_start_in_same_month = 0															
+and district_state in ('MS','FL')															
+and enforcedaterestrictions = 'y'															
+and skuname ilike '%easy%tech%' then studentcount															
+else 0															
+end															
+) as state_initiative_studentcount_restricted															
+from t															
+group by															
+mon_lastday,															
+mon,															
+schoolyear,															
+schoolyear_mon,															
+state_initiative,															
+district_state,															
+organization_district_id,															
+skuname															
+)															
+/*select * 															
+from district_sku_license_data 															
+where mon_lastday ='2025-09-30'*/															
+,district_license_data as (															
+select															
+mon_lastday,															
+mon,															
+schoolyear,															
+schoolyear_mon,															
+district_state,															
+state_initiative,															
+organization_district_id,															
+case															
+when state_initiative then sum(state_initiative_studentcount)															
+else 0															
+end State_Initiative_Number_Of_Students,															
+case															
+when state_initiative then sum(state_initiative_studentcount_restricted)															
+else 0															
+end State_Initiative_Number_Of_Students_restricted,															
+max(sum_studentcount) Number_Of_Students,															
+max(sum_studentcount_restricted) Number_Of_Students_restricted															
+from district_sku_license_data data															
+group by															
+mon_lastday,															
+mon,															
+schoolyear,															
+schoolyear_mon,															
+district_state,															
+state_initiative,															
+organization_district_id															
+),															
+district_data as (															
+select															
+ld.mon_lastday,															
+ld.mon,															
+ld.schoolyear,															
+ld.schoolyear_mon,															
+ld.district_state State,															
+ld.state_initiative,															
+ld.organization_district_id,															
+ld.State_Initiative_Number_Of_Students,															
+ld.State_Initiative_Number_Of_Students_restricted,															
+ld.Number_Of_Students,															
+Ld.Number_Of_Students_restricted															
+from district_license_data ld															
+),															
+--schools from all active orders in the month, not for SKU with max students selected as Number_Of_Students 															
+school_data as															
+(															
+select distinct															
+rld.mon_lastday,															
+rld.mon,															
+rld.schoolyear,															
+rld.schoolyear_mon,															
+rld.district_state State,															
+rld.state_initiative,															
+rld.organization_district_id,															
+rld.organization_school_id,															
+dd.State_Initiative_Number_Of_Students,															
+dd.State_Initiative_Number_Of_Students_restricted,															
+dd.Number_Of_Students,															
+dd.Number_Of_Students_restricted,															
+a.ishighschool															
+from raw_license_data rld															
+join district_data dd															
+on rld.mon_lastday = dd.mon_lastday															
+and rld.organization_district_id = dd.organization_district_id															
+join dw.common.dim_account a															
+on a.account_id=rld.organization_school_id															
+)															
+,cnt_schools as															
+(															
+select															
+mon_lastday,															
+organization_district_id,															
+count( distinct organization_school_id) cnt_schools ,															
+count( case when ishighschool then organization_school_id end) cnt_high_schools															
+from school_data															
+group by															
+mon_lastday,															
+organization_district_id															
+)															
+,data as (															
+select															
+sd.mon_lastday,															
+sd.mon,															
+sd.schoolyear,															
+sd.schoolyear_mon,															
+sd.State,															
+sd.state_initiative,															
+sd.organization_district_id,															
+sd.organization_school_id,															
+cs.cnt_schools,															
+cs.cnt_high_schools,															
+0 State_Initiative_Number_Of_Students,															
+0 State_Initiative_Number_Of_Students_restricted,															
+0 Number_Of_Students,															
+0 Number_Of_Students_Restricted,															
+case															
+when sd.State_Initiative_Number_Of_Students>0 then															
+case															
+when sd.State in ('NC','MI','SC','WV','GA') and not(sd.ishighschool) then															
+sd.State_Initiative_Number_Of_Students/nullif((cs.cnt_schools - cs.cnt_high_schools),0)															
+when sd.State in ('MS','FL') then															
+sd.State_Initiative_Number_Of_Students/nullif(cs.cnt_schools,0)															
+else 0															
+end															
+else 0															
+end State_Initiative_Number_Of_Students_School,															
+case															
+when sd.State_Initiative_Number_Of_Students_restricted>0 then															
+case															
+when sd.State in ('NC','MI','SC','WV','GA') and not(sd.ishighschool) then															
+sd.State_Initiative_Number_Of_Students_restricted/nullif((cs.cnt_schools - cs.cnt_high_schools),0)															
+when sd.State in ('MS','FL') then															
+sd.State_Initiative_Number_Of_Students_restricted/nullif(cs.cnt_schools,0)															
+else 0															
+end															
+else 0															
+end State_Initiative_Number_Of_Students_School_restricted,															
+sd.Number_Of_Students/nullif(cs.cnt_schools,0) Number_Of_Students_School,															
+sd.Number_Of_Students_restricted/nullif(cs.cnt_schools,0) Number_Of_Students_School_restricted															
+from school_data sd															
+join cnt_schools cs															
+on sd.organization_district_id = cs.organization_district_id															
+and sd.mon_lastday = cs.mon_lastday															
+union all															
+select															
+dd.mon_lastday,															
+dd.mon,															
+dd.schoolyear,															
+dd.schoolyear_mon,															
+dd.State,															
+dd.state_initiative,															
+dd.organization_district_id,															
+dd.organization_district_id organization_school_id,															
+cs.cnt_schools,															
+cs.cnt_high_schools,															
+dd.State_Initiative_Number_Of_Students,															
+dd.State_Initiative_Number_Of_Students_restricted,															
+dd.Number_Of_Students,															
+dd.Number_Of_Students_Restricted,															
+0 State_Initiative_Number_Of_Students_School,															
+0 State_Initiative_Number_Of_Students_School_restricted,															
+0 as Number_Of_Students_School,															
+0 as Number_Of_Students_restricted															
+from district_data dd															
+join cnt_schools cs															
+on dd.organization_district_id = cs.organization_district_id															
+and dd.mon_lastday = cs.mon_lastday															
+)															
+select *															
+from data															
+															
