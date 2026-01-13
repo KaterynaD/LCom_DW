b2923896ff68369a983e0bcf8d953cd0a1350a42
Kate Drogaieva <kdrogaieva@learning.com>
2026-01-13 10:18:17 -0800

    Enrollment in DW view

---
+++ dbt/LCom_DW/models/reporting/Views/vw_lcom_org_enrollment.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='view', bind=False) }}
+
+select 
+schoolyear, 
+schoolyear_mon, 
+mon_year, 
+mon_lastday,  
+organization_district_id, 
+organization_school_id, 
+enrollment
+from {{ ref("fact_enrollment_monthly_snapshots") }} fems
