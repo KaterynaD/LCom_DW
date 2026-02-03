a538e99e6afc733d25408dfff69902870e822a5e
Kate Drogaieva <kdrogaieva@learning.com>
2026-02-03 13:19:57 -0800

    excluding view removed from dbt test

--- airflow/utils/dbt_run_utils.py
+++ airflow/utils/dbt_run_utils.py
@@ -226,7 +226,7 @@ def make_run_tests_task(dag, **operator_kwargs) -> BashOperator:
         bash_command=(
             f"cd {DBT_LCOM_DW_PROJECT_DIR} && "
             "dbt test "
-            "--exclude \"config.materialized:view\" \"tag:product_usage\" "
+            "--exclude  \"tag:product_usage\" "
             "--vars '{\"run_type\": \"Scheduled Prod test\"}'"
         ),
         on_failure_callback=notify_task_failure,
