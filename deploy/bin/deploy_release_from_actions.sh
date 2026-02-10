eeb587050f17a7b19ab118636819b1cd2ee38eb7
Kate Drogaieva <kdrogaieva@learning.com>
2026-02-10 14:45:26 -0800

    more experiments with dbt list dbt list --select state:modified

--- deploy/bin/deploy_release_from_actions.sh
+++ deploy/bin/deploy_release_from_actions.sh
@@ -285,6 +285,9 @@ fi
 
 # List modified models ONLY if we have a state manifest to compare against
 if [[ -f "\$STATE_DIR/manifest.json" ]]; then
+
+echo "STATE manifest:"; sha256sum "$STATE_DIR/manifest.json"
+echo "CURRENT manifest before list:"; sha256sum "$DBT_TARGET_PATH/manifest.json" || true
   
 \"\$DBT_BIN\" list \
   --select state:modified \
@@ -293,20 +296,14 @@ if [[ -f "\$STATE_DIR/manifest.json" ]]; then
   --target ${DBT_TARGET_NAME} \
   --vars '{"loaddate": "1900-01-01"}'
 
+echo "STATE manifest:"; sha256sum "$STATE_DIR/manifest.json"
+echo "CURRENT manifest before list:"; sha256sum "$DBT_TARGET_PATH/manifest.json" || true  
+
 else
   echo '[container] Skipping: dbt list --select state:modified (missing '"\$STATE_DIR"'/manifest.json)'
 fi
 
-\"\$DBT_BIN\" compile --target ${DBT_TARGET_NAME}
 
-\"\$DBT_BIN\" docs generate --static --target ${DBT_TARGET_NAME}
-
-echo '[container] Running colibri...'
-colibri generate \
-  --manifest \"\$DBT_TARGET_PATH/manifest.json\" \
-  --catalog  \"\$DBT_TARGET_PATH/catalog.json\" \
-  --output-dir \"\$DBT_TARGET_PATH\"
-"
 
 log "Running dev_check_dags.py and validating output..."
 DAG_CHECK_OUTPUT="$(docker compose exec -T "$SERVICE_NAME" bash -c "
