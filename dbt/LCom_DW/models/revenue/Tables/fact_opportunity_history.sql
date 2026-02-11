db2d07a922686cf4540a010160c92a4df7b7a41c
Kate Drogaieva <kdrogaieva@learning.com>
2026-02-11 11:35:00 -0800

    number_of_schools and number_of_students added

--- dbt/LCom_DW/models/revenue/Tables/fact_opportunity_history.sql
+++ dbt/LCom_DW/models/revenue/Tables/fact_opportunity_history.sql
@@ -14,7 +14,7 @@
 'true_arr',
 'true_arr_formula','true_renewal_arr','last_modified_date',
 'invoiced_date','close_date','start_date','end_date','opp_record_type',
-'license_unenforced','disable_auto_renewal_opp'],
+'license_unenforced','disable_auto_renewal_opp','number_of_schools','number_of_students'],
 
    punch_thru_cols=['account_id'],
 
@@ -72,6 +72,8 @@ end_date ,
 opp_record_type ,
 case when license_unenforced then 1 else 0 end  as license_unenforced ,
 case when disable_auto_renewal_opp then 1 else 0 end as disable_auto_renewal_opp ,
+number_of_schools,
+number_of_students,
 last_modified_date 
 from {{ ref("fact_opportunity") }}
 {% if is_incremental() %}
