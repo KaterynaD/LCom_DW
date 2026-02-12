11127d461764bcf6b4727fd4509fbfff2361bd15
Kate Drogaieva <kdrogaieva@learning.com>
2026-02-12 08:33:22 -0800

    Test DAG

--- airflow/dags/test_email_summary_dag.py
+++ airflow/dags/test_email_summary_dag.py
@@ -4,7 +4,13 @@ from airflow.operators.python import PythonOperator
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.utils.email import send_email
 
+from airflow.models import Variable
 
+import pendulum
+
+local_tz = pendulum.timezone("America/Los_Angeles")
+
+SCHEDULE_TEST_RUN = Variable.get("SCHEDULE_TEST_RUN", default_var="0 * * * *")
 # ------------------------------------------------------------------------
 # Dummy summary function (always runs)
 # ------------------------------------------------------------------------
@@ -32,12 +38,24 @@ def send_test_summary(**context):
 # ------------------------------------------------------------------------
 # Test DAG — only one task
 # ------------------------------------------------------------------------
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "retries": 0
+}
+
 with DAG(
     dag_id="test_email_summary",
-    start_date=datetime(2024, 1, 1),
+    default_args=default_args,
+    start_date=datetime(2025, 1, 1, tzinfo=local_tz),
+    schedule=SCHEDULE_TEST_RUN,     
+    max_active_runs=1, 
     catchup=False,
     tags=["test", "email"],
 ) as dag:
+
 
     notify_summary = PythonOperator(
         task_id="notify_summary",
