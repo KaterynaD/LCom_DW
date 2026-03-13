362fa997a86a24376a3bac5bf6ce3c4d87ba9010
Kate Drogaieva <kdrogaieva@learning.com>
2026-03-13 14:02:50 -0700

    Fix in fact_order_license model name

--- dbt/LCom_DW/tests/licensing/dim_license_order_school/dim_license_order_school_all_schools_included.sql
+++ dbt/LCom_DW/tests/licensing/dim_license_order_school/dim_license_order_school_all_schools_included.sql
@@ -1,7 +1,7 @@
 select   lower(ord_sch.schoolid) +'___'+ ord_sch.orderid
-from {{ ref("fact_license_orders") }}  stg
+from {{ ref("fact_license_order") }}  stg
 join {{ source("staging","license_orderschool") }} ord_sch
-on stg.orderid = ord_sch.orderid
+on stg.order_id = ord_sch.orderid
 join {{ source("dbo","organization") }} o
 on lower(ord_sch.schoolid) = o.organization_id
 where ord_sch.Valid_boolean=true
