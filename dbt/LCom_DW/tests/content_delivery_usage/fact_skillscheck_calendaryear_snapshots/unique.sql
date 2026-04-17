fe51a4ca1609ca8ee898cf78739a56da099b2ac2
Aaron Butler <abutler@learning.com>
2026-04-17 14:09:37 -0700

    RA-424 Add tests for fact_skillscheck tables

---
+++ dbt/LCom_DW/tests/content_delivery_usage/fact_skillscheck_calendaryear_snapshots/unique.sql
@@ -0,0 +1,11 @@
+-- Uniqueness test for the configured unique_key
+-- Ensures no duplicate rows for the unique grain
+select
+  schoolyear,
+  organization_district_id,
+  user_account_id,
+  assessment_set_id,
+  count(*) as row_count
+from {{ ref('fact_skillscheck_calendaryear_snapshots') }}
+group by 1,2,3,4
+having count(*) > 1
