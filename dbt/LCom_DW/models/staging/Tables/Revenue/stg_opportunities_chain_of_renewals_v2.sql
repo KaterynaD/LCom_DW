42cafe4839a58cf9aaa4f06b841bafe6b47925a2
Kate Drogaieva <kdrogaieva@learning.com>
2026-05-04 15:38:02 -0700

    reverting back CI/CD test code

--- dbt/LCom_DW/models/staging/Tables/Revenue/stg_opportunities_chain_of_renewals_v2.sql
+++ dbt/LCom_DW/models/staging/Tables/Revenue/stg_opportunities_chain_of_renewals_v2.sql
@@ -4,6 +4,5 @@
 ) }}
 
 -- depends_on: {{ ref('stg_valid_opportunities') }} 
--- depends_on: {{ ref("dim_calendar") }}
 
 call {{ target.database }}.{{ schema }}.processing_opportunities_chain_of_renewals_v2(cast('{{ var("loaddate") }}' as timestamp));
