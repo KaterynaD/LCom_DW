1e4860cc521d850df8602ec40e9c81bbcb4e6790
Kate Drogaieva <kdrogaieva@learning.com>
2026-05-15 15:40:45 -0700

    Product added in FACT_Customers

--- dbt/LCom_DW/models/reporting/Views/vw_customers_monthly.sql
+++ dbt/LCom_DW/models/reporting/Views/vw_customers_monthly.sql
@@ -3,7 +3,7 @@
 )
  }}
 
-select
+select distinct
  f.arr_type
 ,f.mon_year
 ,f.mon_lastday
@@ -12,16 +12,23 @@ select
 ,f.record_type
 ,f.sfdc_ultimate_parent_id
 ,f.BizDevFlg
+,p.sfdc_product_sub_family product
 ,ah.sfdc_billing_state state
 ,ah.sfdc_billing_country country
+,a.sfdc_customer_level
 ,ah.sfdc_name
 ,case when ah.sfdc_district_enrollment=0 then ah.sfdc_school_enrollment else ah.sfdc_district_enrollment end as district_enrollment
 ,case when (ah.sfdc_state_initiative or ah.sfdc_state_initiative_school) then true else false end as state_initiative
 ,ah.sfdc_urban_rural as urban_rural
 from {{ ref("fact_customers") }} f
+join {{ ref("dim_sfdc_product") }} p
+on f.sfdc_product_id = p.sfdc_product_id
 join {{ ref("dim_account_history") }} ah
 on f.sfdc_ultimate_parent_id = ah.sfdc_account_id
 and f.mon_lastday between ah.fromdate and ah.todate
+--
+join {{ ref("dim_account") }} a
+on f.sfdc_ultimate_parent_id = a.sfdc_account_id
 --
 join {{ ref("dim_month") }} c
 on f.mon_year=c.mon_year
--- dbt/LCom_DW/models/revenue/Tables/fact_customers.sql
+++ dbt/LCom_DW/models/revenue/Tables/fact_customers.sql
@@ -26,11 +26,14 @@ f.bucket,
 f.mon_year,
 f.mon_lastday,
 f.fiscalyear,
+f.sfdc_product_id,
 a.sfdc_ultimate_parent_id,
+case when f.bucket ilike '%biz dev%' then 'State' else 'District' end as BizDevFlg,
 sum(f.arr_amount) amount
 from {{ ref('fact_arr') }} f
 join {{ ref('dim_account') }} a
 on f.account_id = a.account_id
+where GetDate() between f.arr_activation_date and f.arr_deactivation_date
 group by all
 )
 /*---------------------------------------------------------------------------------------------------------------------------------------*/
@@ -41,6 +44,8 @@ arr_type,
 mon_year,
 mon_lastday,
 fiscalyear,
+sfdc_product_id,
+BizDevFlg,
 sfdc_ultimate_parent_id,
 sum(amount) amount
 from vw_fact_revenue_monthly_snapshots
@@ -50,26 +55,17 @@ having sum(amount)>0
 )
 /*---------------------------------------------------------------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------------------------------------------------*/
-,customers_category as (
-select
-mon_year,
-mon_lastday,
-fiscalyear,
-sfdc_ultimate_parent_id,
-max(case when bucket ilike '%biz dev%' then 'State' else 'District' end) BizDevFlg
-from vw_fact_revenue_monthly_snapshots
-where record_type='ARR'
-group by all
-)
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
 ,contract_based_active_data as (
 select
 d.mon_year,
 d.mon_lastday,
 d.fiscalyear,
+ol.sfdc_product_id,
+case when ol.business_type_opty_product ilike '%biz dev%' then 'State' else 'District' end as BizDevFlg,
 a.sfdc_ultimate_parent_id
 from {{ ref('fact_opportunity') }} o
+join {{ ref('dim_opportunity_line') }} ol
+on o.opportunity_id = ol.opportunity_id
 join {{ ref('dim_account') }} a
 on o.account_id = a.account_id
 --Won, invoiced opportunities active at in the month
@@ -80,6 +76,10 @@ o.stage_name ilike '%won%'
 and o.invoiced_date!='1900-01-01'
 and o.invoiced_date <= GetDate()
 and d.mon_year <= to_char(GetDate(),'yyyymm')
+and (
+ol.business_type_opty_product ilike '%arr%' or
+ol.business_type_opty_product ilike '%biz_dev%'
+)
 group by all
 having sum(o.amount)>0
 )
@@ -91,6 +91,8 @@ vfrms.arr_type,
 vfrms.mon_year,
 vfrms.mon_lastday,
 vfrms.fiscalyear,
+vfrms.sfdc_product_id,
+vfrms.BizDevFlg,
 vfrms.sfdc_ultimate_parent_id,
 'Churn' record_type
 from
@@ -100,6 +102,8 @@ left outer join active_paying_customers ah
 on ah.sfdc_ultimate_parent_id = vfrms.sfdc_ultimate_parent_id
 and vfrms.mon_year = ah.mon_year
 and vfrms.arr_type = ah.arr_type
+and vfrms.sfdc_product_id = ah.sfdc_product_id
+and vfrms.BizDevFlg = ah.BizDevFlg
 -- 
 where vfrms.record_type='ARR-MonthlyReduced'
 and vfrms.bucket like '%Cancel%'
@@ -116,6 +120,8 @@ vfrms.arr_type,
 vfrms.mon_year,
 vfrms.mon_lastday,
 vfrms.fiscalyear,
+vfrms.sfdc_product_id,
+vfrms.BizDevFlg,
 vfrms.sfdc_ultimate_parent_id,
 'Expiration' record_type
 from
@@ -125,6 +131,8 @@ left outer join active_paying_customers ah
 on ah.sfdc_ultimate_parent_id = vfrms.sfdc_ultimate_parent_id
 and vfrms.mon_year = ah.mon_year
 and vfrms.arr_type = ah.arr_type
+and vfrms.sfdc_product_id = ah.sfdc_product_id
+and vfrms.BizDevFlg = ah.BizDevFlg
 where vfrms.record_type='ARR-MonthlyReduced'
 and vfrms.bucket like '%Expir%'
 and ah.sfdc_ultimate_parent_id is null
@@ -140,6 +148,8 @@ vfrms.arr_type,
 vfrms.mon_year,
 vfrms.mon_lastday,
 vfrms.fiscalyear,
+vfrms.sfdc_product_id,
+vfrms.BizDevFlg,
 vfrms.sfdc_ultimate_parent_id,
 'Starting' record_type
 from
@@ -157,6 +167,8 @@ vfrms.arr_type,
 vfrms.mon_year,
 vfrms.mon_lastday,
 vfrms.fiscalyear,
+vfrms.sfdc_product_id,
+vfrms.BizDevFlg,
 vfrms.sfdc_ultimate_parent_id
 from
 vw_fact_revenue_monthly_snapshots vfrms
@@ -173,6 +185,8 @@ vfrms.arr_type,
 vfrms.FiscalYear,
 vfrms.mon_year,
 vfrms.mon_lastday,
+vfrms.sfdc_product_id,
+vfrms.BizDevFlg,
 vfrms.sfdc_ultimate_parent_id,
 case
 --first FY customer
@@ -188,12 +202,16 @@ left outer join Starting s
 on vfrms.sfdc_ultimate_parent_id = s.sfdc_ultimate_parent_id
 and vfrms.fiscalyear = s.fiscalyear
 and vfrms.arr_type = s.arr_type
+and vfrms.sfdc_product_id = s.sfdc_product_id
+and vfrms.BizDevFlg = s.BizDevFlg
 -- 
 --if included in MonthlyAdded it is not New or Returning, unless it was Churned after MonthlyAdded
 left outer join MonthlyAdded ma
 on vfrms.sfdc_ultimate_parent_id = ma.sfdc_ultimate_parent_id
 and vfrms.fiscalyear = ma.fiscalyear
 and vfrms.arr_type = ma.arr_type
+and vfrms.sfdc_product_id = ma.sfdc_product_id
+and vfrms.BizDevFlg = ma.BizDevFlg
 and vfrms.mon_year > ma.mon_year
 -- 
 --Was it Churned or Expired this fiscalyear? 
@@ -202,14 +220,18 @@ left outer join
 select
 data.arr_type,
 fiscalyear,
+data.sfdc_product_id,
+data.BizDevFlg,
 sfdc_ultimate_parent_id,
 max(mon_lastday) last_churned_mon_lastday
-from (select arr_type, fiscalyear, sfdc_ultimate_parent_id, mon_lastday from Churn union all select arr_type, fiscalyear, sfdc_ultimate_parent_id, mon_lastday from Expired) data
+from (select arr_type, fiscalyear, sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id, mon_lastday from Churn union all select arr_type, fiscalyear, sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id, mon_lastday from Expired) data
 group by all
 ) c
 on vfrms.sfdc_ultimate_parent_id = c.sfdc_ultimate_parent_id
 and vfrms.fiscalyear = c.fiscalyear
 and vfrms.arr_type = c.arr_type
+and vfrms.sfdc_product_id = c.sfdc_product_id
+and vfrms.BizDevFlg = c.BizDevFlg
 --
 --Was it present in ARR previous fiscal years?
 left outer join
@@ -217,6 +239,8 @@ left outer join
 select
 arr_type,
 fiscalyear,
+sfdc_product_id,
+BizDevFlg,
 sfdc_ultimate_parent_id,
 max(mon_lastday) last_present_mon_lastday
 from vw_fact_revenue_monthly_snapshots
@@ -228,6 +252,8 @@ on vfrms.sfdc_ultimate_parent_id = prev_fy.sfdc_ultimate_parent_id
 and vfrms.fiscalyear != prev_fy.fiscalyear
 and vfrms.mon_lastday > prev_fy.last_present_mon_lastday
 and vfrms.arr_type = prev_fy.arr_type
+and vfrms.sfdc_product_id = prev_fy.sfdc_product_id
+and vfrms.BizDevFlg = prev_fy.BizDevFlg
 --
 -- 
 where vfrms.record_type='ARR-MonthlyAdded'
@@ -248,6 +274,8 @@ FiscalYear,
 mon_year,
 mon_lastday,
 record_type,
+sfdc_product_id,
+BizDevFlg,
 sfdc_ultimate_parent_id
 from new_and_returning_raw
 group by
@@ -262,6 +290,8 @@ s.arr_type,
 m.FiscalYear,
 m.mon_year,
 m.Mon_LastDay,
+s.sfdc_product_id,
+s.BizDevFlg,
 s.sfdc_ultimate_parent_id
 from starting s
 join dim_month m
@@ -272,6 +302,8 @@ s.arr_type,
 m.FiscalYear,
 m.mon_year,
 m.Mon_LastDay,
+s.sfdc_product_id,
+s.BizDevFlg,
 s.sfdc_ultimate_parent_id
 from new_and_returning s
 join dim_month m
@@ -282,11 +314,16 @@ s.arr_type,
 m.FiscalYear,
 m.Mon_Year,
 m.Mon_LastDay,
+s.sfdc_product_id,
+s.BizDevFlg,
 s.sfdc_ultimate_parent_id
-from (select arr_type, fiscalyear, Mon_Year, sfdc_ultimate_parent_id, mon_lastday from Churn union all select arr_type, fiscalyear, Mon_Year, sfdc_ultimate_parent_id, mon_lastday from Expired) s
+from (select arr_type, fiscalyear, Mon_Year, sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id, mon_lastday from Churn union all select arr_type, fiscalyear, Mon_Year, sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id, mon_lastday from Expired) s
 left outer join new_and_returning nr
 on s.sfdc_ultimate_parent_id = nr.sfdc_ultimate_parent_id
 and s.fiscalyear = nr.fiscalyear
+and s.arr_type = nr.arr_type
+and s.sfdc_product_id = nr.sfdc_product_id
+and s.BizDevFlg = nr.BizDevFlg
 and s.mon_year < nr.mon_year
 join dim_month m
 --from the month of Churn till the end of FY (FY Start Date changed when the new FY starts and churn month is less then it)
@@ -303,6 +340,8 @@ s.arr_type,
 m.FiscalYear,
 m.mon_year,
 m.Mon_LastDay,
+s.sfdc_product_id,
+s.BizDevFlg,
 s.sfdc_ultimate_parent_id
 from starting s
 join dim_month m
@@ -313,6 +352,8 @@ s.arr_type,
 m.FiscalYear,
 m.mon_year,
 m.Mon_LastDay,
+s.sfdc_product_id,
+s.BizDevFlg,
 s.sfdc_ultimate_parent_id
 from new_and_returning s
 join dim_month m
@@ -321,23 +362,23 @@ on s.mon_lastday between m.FiscalYear_StartDate and m.Mon_LastDay
 /*---------------------------------------------------------------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------------------------------------------------*/
 ,rawdata as (
-select arr_type, FiscalYear, mon_year,mon_lastday,'Total Active' as record_type,  sfdc_ultimate_parent_id from total_active_data where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
+select arr_type, FiscalYear, mon_year,mon_lastday,'Total Active' as record_type,  sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id from total_active_data where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
 union all
-select arr_type, FiscalYear,mon_year,mon_lastday,'Net Active' as record_type, sfdc_ultimate_parent_id from net_active_data where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
+select arr_type, FiscalYear,mon_year,mon_lastday,'Net Active' as record_type, sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id from net_active_data where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
 union all
-select 'Preliminary' arr_type, FiscalYear,mon_year,mon_lastday,'Contract Active' as record_type,  sfdc_ultimate_parent_id from contract_based_active_data where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
+select 'Preliminary' arr_type, FiscalYear,mon_year,mon_lastday,'Contract Active' as record_type,  sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id from contract_based_active_data where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
 union all
-select 'Backdated' arr_type, FiscalYear,mon_year,mon_lastday,'Contract Active' as record_type,  sfdc_ultimate_parent_id from contract_based_active_data where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
+select 'Backdated' arr_type, FiscalYear,mon_year,mon_lastday,'Contract Active' as record_type,  sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id from contract_based_active_data where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
 union all
-select 'True' arr_type, FiscalYear,mon_year,mon_lastday,'Contract Active' as record_type,  sfdc_ultimate_parent_id from contract_based_active_data where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
+select 'True' arr_type, FiscalYear,mon_year,mon_lastday,'Contract Active' as record_type,  sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id from contract_based_active_data where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
 union all
-select arr_type, FiscalYear,mon_year,mon_lastday,'Starting' as record_type,  sfdc_ultimate_parent_id from starting where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
+select arr_type, FiscalYear,mon_year,mon_lastday,'Starting' as record_type,  sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id from starting where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
 union all
-select arr_type, FiscalYear,mon_year,mon_lastday, record_type,  sfdc_ultimate_parent_id from new_and_returning where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
+select arr_type, FiscalYear,mon_year,mon_lastday, record_type,  sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id from new_and_returning where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
 union all
-select arr_type, FiscalYear,mon_year,mon_lastday, record_type,  sfdc_ultimate_parent_id from churn where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
+select arr_type, FiscalYear,mon_year,mon_lastday, record_type,  sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id from churn where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
 union all
-select arr_type, FiscalYear,mon_year,mon_lastday, record_type,  sfdc_ultimate_parent_id from expired where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
+select arr_type, FiscalYear,mon_year,mon_lastday, record_type,  sfdc_product_id, BizDevFlg, sfdc_ultimate_parent_id from expired where sfdc_ultimate_parent_id!='00000000-0000-0000-0000-000000000000'
 )
 ,data as (
 select
@@ -345,26 +386,25 @@ data.arr_type,
 data.FiscalYear,
 data.mon_year,
 data.mon_lastday,
+data.sfdc_product_id,
+data.BizDevFlg,
 a.account_id,
 data.sfdc_ultimate_parent_id,
-data.record_type,
-isnull(bd.BizDevFlg, 'District') BizDevFlg
+data.record_type
 from rawdata as data
 join dw.common.dim_account a
 on data.sfdc_ultimate_parent_id = a.sfdc_account_id
-left outer join customers_category bd
-on bd.mon_year = data.mon_year
-and bd.sfdc_ultimate_parent_id = data.sfdc_ultimate_parent_id
 )
 select
 arr_type::varchar(20),
 FiscalYear::varchar(20),
 mon_year::integer,
 mon_lastday::date,
+sfdc_product_id::varchar(300),
+BizDevFlg::varchar(10),
 account_id::varchar(300),
 sfdc_ultimate_parent_id::varchar(300),
 record_type::varchar(20),
-BizDevFlg ::varchar(10) ,
 '{{ var("loaddate") }}'::timestamp as loaddate
 from data
 
