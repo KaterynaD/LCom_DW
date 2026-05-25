6e5870d225b2e4f317cdcf71aa7944246df2d99a
Kate Drogaieva <kdrogaieva@learning.com>
2026-05-25 10:47:41 -0700

    View validation test 3

--- dbt/LCom_DW/models/reporting/Views/vw_arr_12_vs_1_start_fy_validation.sql
+++ dbt/LCom_DW/models/reporting/Views/vw_arr_12_vs_1_start_fy_validation.sql
@@ -2,7 +2,7 @@
    bind=False
 )
  }}
-with data_12_fy_mon AS (									
+with data_12_fy_mon as (									
 select	
 mon_year,
 case when sfdc_ultimate_parent_id='00000000-0000-0000-0000-000000000000' then sfdc_account_id else sfdc_ultimate_parent_id end sfdc_ultimate_parent_id,									
@@ -17,7 +17,7 @@ group by
 mon_year,
 case when sfdc_ultimate_parent_id='00000000-0000-0000-0000-000000000000' then sfdc_account_id else sfdc_ultimate_parent_id end									
 )									
-,starting_1_fy_mon AS (									
+,starting_1_fy_mon as (									
 select		
 mon_year,
 case when sfdc_ultimate_parent_id='00000000-0000-0000-0000-000000000000' then sfdc_account_id else sfdc_ultimate_parent_id end sfdc_ultimate_parent_id,
