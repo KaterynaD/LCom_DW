bd31b01052b32e05bfee47b1673579b2c011a86b
Kate Drogaieva <kdrogaieva@learning.com>
2026-05-29 15:37:09 -0700

    CI/CD Test now really broken dag

--- airflow/dags/hello-world.py
+++ airflow/dags/hello-world.py
@@ -10,15 +10,6 @@ def print_hello():
 
 with DAG(
     dag_id='hello_world_dag',
-    start_date=datetime(2023, 1, 1),
-    catchup=False,
-    tags=['example'],
-) as dag:
-    # Define a PythonOperator task that calls the print_hello function
-    ello_task = PythonOperator(
-        task_id='hello_world_task',
-        python_callable=print_hello,
-    )
-
+    star
 
 
