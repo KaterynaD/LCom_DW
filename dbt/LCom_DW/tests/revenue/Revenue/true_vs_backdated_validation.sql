984351a13eac6a8a555960b9f20fb8332698bb2b
Kate Drogaieva <kdrogaieva@learning.com>
2026-06-02 16:25:08 -0700

    Starting date to test date 202907

--- dbt/LCom_DW/tests/revenue/Revenue/true_vs_backdated_validation.sql
+++ dbt/LCom_DW/tests/revenue/Revenue/true_vs_backdated_validation.sql
@@ -1,4 +1,4 @@
 select *
 from {{ ref("vw_true_to_backdated_arr_validation") }}
 /*there is 1.5 - 8.5% difference with negative and replacment opportunities added in 2017/2018 FY*/
-where mon_year >= 202007 and pct_diff>1
+where mon_year >= 201907 and pct_diff>1
