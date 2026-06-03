5bf635bef986e32385e27a757e718783b1684ed7
Kate Drogaieva <kdrogaieva@learning.com>
2026-06-03 09:19:10 -0700

    Limit shifted to 202407 to avoide mass load old opportunities issues

--- dbt/LCom_DW/tests/revenue/Revenue/true_vs_backdated_validation.sql
+++ dbt/LCom_DW/tests/revenue/Revenue/true_vs_backdated_validation.sql
@@ -1,4 +1,4 @@
 select *
 from {{ ref("vw_true_to_backdated_arr_validation") }}
 /*there is 1.5 - 8.5% difference with negative and replacment opportunities added in 2017/2018 FY*/
-where mon_year >= 201807 and pct_diff>1
+where mon_year >= 202407 and pct_diff>1
