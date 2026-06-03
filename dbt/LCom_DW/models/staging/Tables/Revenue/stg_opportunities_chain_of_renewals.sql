c345ae12c31b18860c83332bf25186e1445ba95f
Kate Drogaieva <kdrogaieva@learning.com>
2026-06-03 16:28:51 -0700

    CI/CD test fixed SP + model marked to deploy

--- dbt/LCom_DW/models/staging/Tables/Revenue/stg_opportunities_chain_of_renewals.sql
+++ dbt/LCom_DW/models/staging/Tables/Revenue/stg_opportunities_chain_of_renewals.sql
@@ -1,6 +1,7 @@
 
 {{ config(
    materialized='sql_runner',
+   deployment_date='2026-06-03',
    pre_hook = [
                     '{{ create_stg_opportunities_chain_of_renewals() }}', 
                     '{{ create_processing_opportunities_chain_of_renewals() }}'
