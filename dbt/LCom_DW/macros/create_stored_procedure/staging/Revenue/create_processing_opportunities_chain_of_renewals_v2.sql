ec803e1cb5f285dc4189df0f8385be69691e431f
Kate Drogaieva <kdrogaieva@learning.com>
2026-06-03 16:44:54 -0700

    CI/CD test deploying modified SP

--- dbt/LCom_DW/macros/create_stored_procedure/staging/Revenue/create_processing_opportunities_chain_of_renewals_v2.sql
+++ dbt/LCom_DW/macros/create_stored_procedure/staging/Revenue/create_processing_opportunities_chain_of_renewals_v2.sql
@@ -23,6 +23,8 @@ batch_count int;
 offset_value int;
 BEGIN
 
+--v2 for new ARR recognition
+
 RAISE INFO 'Creating temp table with list of renewable opportunities...';
 drop table if exists temp_opportunities_ids;
 create temporary table temp_opportunities_ids as
