ebfa640af32e5c6a289f80361a440770024f6a58
Kate Drogaieva <kdrogaieva@learning.com>
2026-06-03 16:21:20 -0700

    CI/CD test fixed SP - deploy in Prod

--- dbt/LCom_DW/macros/create_stored_procedure/staging/Revenue/create_processing_opportunities_chain_of_renewals.sql
+++ dbt/LCom_DW/macros/create_stored_procedure/staging/Revenue/create_processing_opportunities_chain_of_renewals.sql
@@ -26,7 +26,7 @@ batch_count int;
 offset_value int;					
 BEGIN					
 
-Stored procedure to be replaced with v2 soon
+--Stored procedure to be replaced with v2 soon
 
 RAISE INFO 'Creating temp table with list of renewable opportunities...';					
 drop table if exists temp_opportunities_ids;					
