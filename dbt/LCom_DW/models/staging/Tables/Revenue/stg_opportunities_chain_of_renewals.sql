9776e6348e658b60dadfa2f501e7df8058d31f10
Kate Drogaieva <kdrogaieva@learning.com>
2026-06-03 17:02:19 -0700

    CI/CD change in table which depends on SP

--- dbt/LCom_DW/models/staging/Tables/Revenue/stg_opportunities_chain_of_renewals.sql
+++ dbt/LCom_DW/models/staging/Tables/Revenue/stg_opportunities_chain_of_renewals.sql
@@ -1,7 +1,6 @@
 
 {{ config(
    materialized='sql_runner',
-   deployment_date='2026-06-03',
    pre_hook = [
                     '{{ create_stg_opportunities_chain_of_renewals() }}', 
                     '{{ create_processing_opportunities_chain_of_renewals() }}'
