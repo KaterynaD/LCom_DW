c9d56731610aa95a17d0888931fe9915b56c1bcc
Kate Drogaieva <kdrogaieva@learning.com>
2026-06-08 16:15:50 -0700

    CI/CD testing clean up

--- dbt/LCom_DW/macros/framework/deployment/post_deployment_tasks.sql
+++ dbt/LCom_DW/macros/framework/deployment/post_deployment_tasks.sql
@@ -7,14 +7,6 @@
 
  {% set post_deployment_sql %}
 
- update staging.d_product
- set created_date = data.created_date
- from (
-    select id, created_date
-    from {{ source('fivetran_salesforce_quickstart', 'product_2') }}
- ) data
- where staging.d_product.id = data.id and
-    staging.d_product.created_date is null;
 
  {% endset %}
 
