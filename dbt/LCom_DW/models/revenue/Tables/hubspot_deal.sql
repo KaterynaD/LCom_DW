59fd4d3501d8298271dc4355af3e9400ba73b6a6
Kate Drogaieva <kdrogaieva@learning.com>
2026-06-16 08:55:24 -0700

    limit linked companies to type 5 - deal_to_company

--- dbt/LCom_DW/models/revenue/Tables/hubspot_deal.sql
+++ dbt/LCom_DW/models/revenue/Tables/hubspot_deal.sql
@@ -8,10 +8,22 @@
         )
 }}
 
+with a as (
+  select 
+    dc.deal_id,
+    max(c.property_name) as property_name --precaution for multiple company names per deal
+  from {{ source('fivetran_hubspot', 'company') }} c
+  join {{ source('fivetran_hubspot', 'deal_company') }}  dc
+  on c.id = dc.company_id
+  where dc.type_id = 5 --deal_to_company
+  and c.is_deleted is False
+  and c._fivetran_deleted is false
+  group by dc.deal_id
+)
 select
 d.deal_id::varchar(300),
 isnull(d.property_dealname,'{{ var("default_varchar") }}')::varchar(256) deal_name,
-isnull(c.property_name,'Unknown')::varchar(256) account_name,
+isnull(a.property_name,'Unknown')::varchar(256) account_name,
 isnull(d.property_billing_state_c,'Unknown')::varchar(256) billing_state,
 isnull((d.property_cart_purchased_date AT TIME ZONE 'utc'), '{{ var("default_date") }}')::date as purchased_date,
 isnull(d.property_amount,{{ var("default_numeric") }})::numeric(35,10) as amount,
@@ -23,12 +35,8 @@ isnull(replace(property_ecommerce_invoice_link_sync , 'https://info.learning.com
 'EComm'::varchar(5) deal_type,
 '{{ var("loaddate") }}'::TIMESTAMP WITHOUT TIME ZONE as loaddate
 from {{ source('fivetran_hubspot', 'deal') }} d
-join {{ source('fivetran_hubspot', 'deal_company') }}  dc
-on d.deal_id = dc.deal_id
-join {{ source('fivetran_hubspot', 'company') }}  c
-on dc.company_id = c.id
+left outer join a
+on d.deal_id = a.deal_id
 where (d.property_ecommerce_cart is True or d.property_ecommerce_renewal is True)
 and d.is_deleted is False
 and d._fivetran_deleted is false
-and c.is_deleted is False
-and c._fivetran_deleted is false
