63d35d7aef541b4e37a3d48afde3ca1cd1ee6cce
Kate Drogaieva <kdrogaieva@learning.com>
2026-06-30 10:22:18 -0700

    direct table  in pre deployment task

--- dbt/LCom_DW/macros/framework/deployment/pre_deployment_tasks.sql
+++ dbt/LCom_DW/macros/framework/deployment/pre_deployment_tasks.sql
@@ -12,6 +12,7 @@
  {% if target_db | upper == 'QA' %}
 
    create table qa.revenue.fact_opportunity_history as select * from dw.revenue.fact_opportunity_history;
+   create table qa.revenue.fact_opportunity as select * from dw.revenue.fact_opportunity;
 
  {% endif %} 
 					
@@ -21,7 +22,7 @@ alter table revenue.fact_opportunity_history add column multi_year_discount_rate
 update revenue.fact_opportunity_history
 set
 multi_year_discount_rate = fo.multi_year_discount_rate
-from {{ ref('fact_opportunity') }} fo
+from revenue.fact_opportunity fo
 where fo.opportunity_id=revenue.fact_opportunity_history.opportunity_id;
 
 
