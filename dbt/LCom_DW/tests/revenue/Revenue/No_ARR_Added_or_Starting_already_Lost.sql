eca352ee763d30770eb27e4a5d0975c8c7659757
Kate Drogaieva <kdrogaieva@learning.com>
2026-07-06 09:47:42 -0700

    Old ARR active renewal based framework removed

--- dbt/LCom_DW/tests/revenue/Revenue/No_ARR_Added_or_Starting_already_Lost.sql
+++
@@ -1,5 +0,0 @@
-select distinct opportunity_id
-from {{ ref("fact_revenue_monthly_snapshots") }}
-where record_type!='MonthlyBooking'
-and renewal_stage_name = 'Closed Lost'
-and mon_year > to_char(renewal_close_date,'yyyymm')
