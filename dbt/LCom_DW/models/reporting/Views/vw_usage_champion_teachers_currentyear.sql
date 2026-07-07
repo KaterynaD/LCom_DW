5759aae17a1757db9cd0317914043486acd39681
Aaron Butler <abutler@learning.com>
2026-07-06 18:00:52 -0700

    RA-456 Use SFDC district and school names if not 'Unknown' and exclude 'Unknown' schools

--- dbt/LCom_DW/models/reporting/Views/vw_usage_champion_teachers_currentyear.sql
+++ dbt/LCom_DW/models/reporting/Views/vw_usage_champion_teachers_currentyear.sql
@@ -14,11 +14,11 @@ select
     org.sfdc_state_program_eligible as state_program_eligible,
     org.sfdc_state_initiative as state_initiative,
     org.lcom_district_id as district_id,
-    org.lcom_district_name as district_name,
+    coalesce(nullif(org.sfdc_district_name, 'Unknown'), org.lcom_district_name) as district_name,
     org.lcom_demo as is_demo_district,
     org.lcom_trial as is_trial_district,
     sch.lcom_school_id as school_id,
-    sch.lcom_school_name as school_name,
+    coalesce(nullif(sch.sfdc_school_name, 'Unknown'), sch.lcom_school_name) as school_name,
     fal.user_primary_teacher_id as teacher_id,
     coalesce(tch.first_name + ' ' + tch.last_name, lp.learning_pathway_name) as teacher_name,
     tch.email as teacher_email,
@@ -28,9 +28,11 @@ select
     count(distinct fal.user_account_id) as num_users
 from {{ source("dbo","fact_assignment_launch") }}  as fal
 inner join {{ ref("dim_district") }} as org
-    on fal.organization_district_id = org.district_id
+    on fal.organization_district_id = org.lcom_district_id
 inner join {{ ref("dim_school") }} as sch
-    on fal.organization_school_id = sch.school_id
+    on
+        fal.organization_school_id = sch.lcom_school_id
+        and fal.organization_district_id = sch.lcom_district_id
 inner join {{ ref("dim_calendar") }} as cal
     on cast(fal.launch_datetime as DATE) = cal.cal_date
 left join {{ source("dbo","mv_teacher_account") }} as tch
@@ -42,6 +44,8 @@ left join {{ source("dbo","learning_pathway") }} as lp
 where current_date between cal.schoolyear_startdate and dateadd(year, 1, cal.schoolyear_enddate)
 group by
     cal.schoolyear, org.lcom_country_code, org.lcom_state_province_key, org.lcom_state_province_name, org.sfdc_state_program_eligible, org.sfdc_state_initiative,
-    org.lcom_district_id, org.lcom_district_name, org.lcom_demo, org.lcom_trial, sch.lcom_school_id, sch.lcom_school_name,
+    org.lcom_district_id, org.sfdc_district_name, org.lcom_district_name, org.lcom_demo, org.lcom_trial,
+    sch.lcom_school_id, sch.sfdc_school_name, sch.lcom_school_name,
     fal.user_primary_teacher_id, coalesce(tch.first_name + ' ' + tch.last_name, lp.learning_pathway_name), tch.email,
     case when fal.learning_pathway_id <> '00000000-0000-0000-0000-000000000000' then 'Pathway' else 'Teacher-Assigned' end
+having coalesce(nullif(sch.sfdc_school_name, 'Unknown'), sch.lcom_school_name) <> 'Unknown'
