703422a09973c8936968cb9cc335f50c6cba8faa
Kate Drogaieva <kdrogaieva@learning.com>
2026-07-09 08:23:37 -0700

    Fivetran added new columns without underscore "_" in column names with trailing spaces and recommended do not use the old column names

--- dbt/LCom_DW/models/revenue/Tables/netsuite_ch042808.sql
+++ dbt/LCom_DW/models/revenue/Tables/netsuite_ch042808.sql
@@ -13,22 +13,22 @@
 
 with data as (
 select
-replace(replace(Class_::varchar(500), '> ', '>'), ' >', '>') as business_class,
-Primary_Sales_Rep_::varchar(500) as primary_sales_rep,
+replace(replace("class"::varchar(500), '> ', '>'), ' >', '>') as business_class,
+Primary_Sales_Rep::varchar(500) as primary_sales_rep,
 Name_grouped_::varchar(500) as name,
-Bill_To_::varchar(500) as bill_to,
-customer_implementation_target_level_::varchar(500) as target_level,
+Bill_To::varchar(500) as bill_to,
+customer_implementation_target_level::varchar(500) as target_level,
 item_name_grouped_::varchar(500) as item,
-Progressive_Billing_::varchar(500) as progressive_billing,
-Memo_::varchar(65535) as memo,
-rev_rec_start_date_::date as start_date,
-rev_rec_end_date_::date as end_date,
-transaction_number_::varchar(500) as transaction_number,
-date_::date as invoiced_date,
-replace(replace(replace(replace(sales_, '$', ''), ',', ''), '(', '-'), ')', '')::double precision as sales,
-Created_From_::varchar(100) as created_from,
-sf_net_suite_order_id_::varchar(500) as sf_netsuite_order_id,
-address_billing_address_state_::varchar(100) as billing_state
+Progressive_Billing::varchar(500) as progressive_billing,
+Memo::varchar(65535) as memo,
+rev_rec_start_date::date as start_date,
+rev_rec_end_date::date as end_date,
+transaction_number::varchar(500) as transaction_number,
+date::date as invoiced_date,
+replace(replace(replace(replace(sales, '$', ''), ',', ''), '(', '-'), ')', '')::double precision as sales,
+Created_From::varchar(100) as created_from,
+sf_net_suite_order_id::varchar(500) as sf_netsuite_order_id,
+address_billing_address_state::varchar(100) as billing_state
 from {{ source('fivetran_email', 'ch_042808') }}
 where _modified = (select max(_modified) from {{ source('fivetran_email', 'ch_042808') }})
 )
