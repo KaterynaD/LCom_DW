1207e4dddfec6728e85214e64bbf903647fefec6
Kate Drogaieva <kdrogaieva@learning.com>
2026-07-20 11:43:58 -0700

    switch to a standard ARR view with applied ARR filters

--- dbt/LCom_DW/models/reporting/Views/vw_arr_booking_account.sql
+++ dbt/LCom_DW/models/reporting/Views/vw_arr_booking_account.sql
@@ -6,15 +6,7 @@
 )
  }}
  
-with opportunities_issues as (
-select 
-a.opportunity_id,
-listagg(i.issue, ', ') WITHIN GROUP (ORDER BY i.issue) AS  issues
-from {{ ref('dim_arr_audit') }} a
-join {{ ref('dim_arr_issue') }} i
-on a.issue_id = i.issue_id
-group by a.opportunity_id
-),
+with 
 data as (
 select 
 f.mon_year,
@@ -25,16 +17,11 @@ f.account_id,
 a.sfdc_account_id,
 'ARR' record_type,
 sum(f.arr_amount) as amount
-from {{ ref("fact_arr") }} f
-left outer join opportunities_issues i
-on f.opportunity_id = i.opportunity_id
+from {{ ref("vw_fact_arr") }} f
 join {{ ref("dim_account") }} a
 on f.account_id = a.account_id
 where  f.arr_type = 'Preliminary'
 and f.record_type = 'ARR'
-and getdate() between f.arr_activation_date and f.arr_deactivation_date
-      and isnull(i.issues,'Valid') not ilike '%negative opp%'
-      and isnull(i.issues,'Valid') not ilike '%replacement opp%'  
 group by 
 f.mon_year,
 f.mon_lastday,
