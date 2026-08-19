b5d21fd60f3ef68b1ea7184af11fae644199af0c
Kate Drogaieva <kdrogaieva@learning.com>
2026-08-19 15:23:44 -0700

    create for SP based model needed models as views in QA

--- dbt/LCom_DW/macros/framework/deployment/pre_deployment_tasks.sql
+++ dbt/LCom_DW/macros/framework/deployment/pre_deployment_tasks.sql
@@ -11,59 +11,16 @@
 
  {% if target_db | upper == 'QA' %}
 
-   create table qa.revenue.fact_opportunity_history as select * from dw.revenue.fact_opportunity_history limit 100;
-   create table qa.revenue.fact_opportunity as select * from dw.revenue.fact_opportunity limit 100;
+--deploying stored procedure based model even with deps did not create needed models in QA automatically
+create or replace view qa.common.dim_month as select * from dw.common.dim_month  limit 10 with no schema binding;
+create or replace view qa.common.dim_district as select * from dw.common.dim_month  limit 10 with no schema binding;
+create or replace view qa.content_delivery_usage.dim_learning_object as select * from dw.content_delivery_usage.dim_learning_object  limit 10 with no schema binding;
+create or replace view qa.content_delivery_usage.dim_product_category as select * from dw.content_delivery_usage.dim_product_category limit 10   with no schema binding;
+create or replace view qa.content_delivery_usage.dim_product_category_learning_object_monthly as select * from dw.content_delivery_usage.dim_product_category_learning_object_monthly limit 10 with no schema binding;
 
  {% endif %} 
 					
-alter table revenue.fact_opportunity_history add column owner_id varchar(300) not null default '00000000-0000-0000-0000-000000000000';
 
-update revenue.fact_opportunity_history h
-set owner_id = fo.owner_id
-from revenue.fact_opportunity fo
-where fo.opportunity_id = h.opportunity_id;
-
-update revenue.fact_opportunity_history
-set scd_hash = md5(
-       coalesce(cast(stage_name as varchar), '')
-    || '|' || coalesce(cast(name as varchar), '')
-    || '|' || coalesce(cast(amount as varchar), '')
-    || '|' || coalesce(cast(amount_won as varchar), '')
-    || '|' || coalesce(cast(arr as varchar), '')
-    || '|' || coalesce(cast(arr_new_business as varchar), '')
-    || '|' || coalesce(cast(arr_renewal as varchar), '')
-    || '|' || coalesce(cast(arr_upsell as varchar), '')
-    || '|' || coalesce(cast(arr_won as varchar), '')
-    || '|' || coalesce(cast(combined_arr as varchar), '')
-    || '|' || coalesce(cast(downsell as varchar), '')
-    || '|' || coalesce(cast(multi_year_arr as varchar), '')
-    || '|' || coalesce(cast(new_biz_arr_trigger as varchar), '')
-    || '|' || coalesce(cast(nnarr as varchar), '')
-    || '|' || coalesce(cast(nrr_renewal as varchar), '')
-    || '|' || coalesce(cast(po_amount as varchar), '')
-    || '|' || coalesce(cast(price_increase_arr as varchar), '')
-    || '|' || coalesce(cast(probability as varchar), '')
-    || '|' || coalesce(cast(quote_list_amount as varchar), '')
-    || '|' || coalesce(cast(quote_total_discount as varchar), '')
-    || '|' || coalesce(cast(remaining_quota as varchar), '')
-    || '|' || coalesce(cast(renewable_revenue as varchar), '')
-    || '|' || coalesce(cast(total_arr_bookings as varchar), '')
-    || '|' || coalesce(cast(true_arr as varchar), '')
-    || '|' || coalesce(cast(true_arr_formula as varchar), '')
-    || '|' || coalesce(cast(true_renewal_arr as varchar), '')
-    || '|' || coalesce(cast(last_modified_date as varchar), '')
-    || '|' || coalesce(cast(invoiced_date as varchar), '')
-    || '|' || coalesce(cast(close_date as varchar), '')
-    || '|' || coalesce(cast(start_date as varchar), '')
-    || '|' || coalesce(cast(end_date as varchar), '')
-    || '|' || coalesce(cast(opp_record_type as varchar), '')
-    || '|' || coalesce(cast(license_unenforced as varchar), '')
-    || '|' || coalesce(cast(disable_auto_renewal_opp as varchar), '')
-    || '|' || coalesce(cast(number_of_schools as varchar), '')
-    || '|' || coalesce(cast(number_of_students as varchar), '')
-    || '|' || coalesce(cast(multi_year_discount_rate as varchar), '')
-    || '|' || coalesce(cast(owner_id as varchar), '')
-);
 
  {% endset %}
 
