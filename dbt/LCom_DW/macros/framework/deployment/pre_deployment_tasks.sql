76cdc777f66358bcd591505daf118a6bddeaa778
Kate Drogaieva <kdrogaieva@learning.com>
2026-08-19 15:31:05 -0700

    Fix in qa.common.dim_district

--- dbt/LCom_DW/macros/framework/deployment/pre_deployment_tasks.sql
+++ dbt/LCom_DW/macros/framework/deployment/pre_deployment_tasks.sql
@@ -13,7 +13,7 @@
 
 --deploying stored procedure based model even with deps did not create needed models in QA automatically
 create or replace view qa.common.dim_month as select * from dw.common.dim_month  limit 10 with no schema binding;
-create or replace view qa.common.dim_district as select * from dw.common.dim_month  limit 10 with no schema binding;
+create or replace view qa.common.dim_district as select * from dw.common.dim_district  limit 10 with no schema binding;
 create or replace view qa.content_delivery_usage.dim_learning_object as select * from dw.content_delivery_usage.dim_learning_object  limit 10 with no schema binding;
 create or replace view qa.content_delivery_usage.dim_product_category as select * from dw.content_delivery_usage.dim_product_category limit 10   with no schema binding;
 create or replace view qa.content_delivery_usage.dim_product_category_learning_object_monthly as select * from dw.content_delivery_usage.dim_product_category_learning_object_monthly limit 10 with no schema binding;
