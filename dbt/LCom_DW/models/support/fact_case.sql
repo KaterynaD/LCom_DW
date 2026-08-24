948897af784344547c7977e3566b8a544079e04f
Kate Drogaieva <kdrogaieva@learning.com>
2026-08-24 08:57:00 -0700

    SFDC removed columns removed from DW

--- dbt/LCom_DW/models/support/fact_case.sql
+++ dbt/LCom_DW/models/support/fact_case.sql
@@ -14,12 +14,12 @@ with rawdata as (select
         alias='sfdc_case',
         used_columns=[ 
       'id','account_id','case_number','case_owner_email_c',
-'case_ready_to_survey_c','closed_date','confirmed_resolution_c','contact_id',
-'created_date','csat_response_c','description',
+'closed_date','contact_id',
+'created_date','description',
 'is_closed','is_escalated','last_modified_date',
 'origin','owner_id','platform_name_c','priority',
 'status','subject',
-'survey_send_date_time_c','thread_id_c','type',
+'type',
 'record_type_id','solution_c','is_deleted'
         ],
         profile_src=('profiles','vw_sfdc_schema_audit'),
@@ -35,12 +35,9 @@ isnull(a.account_id, '{{ var("default_ID") }}') as account_id	,
 isnull(stg.account_id, '{{ var("default_ID") }}') as sfdc_account_id	,
 isnull(stg.case_number , '{{ var("default_varchar") }}') as case_number	,
 isnull(stg.case_owner_email_c , '{{ var("default_varchar") }}') as case_owner_email	,
-isnull(stg.case_ready_to_survey_c , {{ var("default_boolean") }}) as case_ready_to_survey	,
 isnull(stg.closed_date ,'{{ var("default_date") }}') as closed_date	,
-isnull(stg.confirmed_resolution_c , {{ var("default_boolean") }}) as confirmed_resolution	,
 isnull(stg.contact_id , '{{ var("default_ID") }}') as contact_id	,
 isnull(stg.created_date	 AT TIME ZONE 'PST','{{ var("default_date") }}') as created_date	,
-isnull(stg.csat_response_c , '{{ var("default_varchar") }}') as csat_response	,
 isnull(stg.description , '{{ var("default_varchar") }}') as description	,
 isnull(stg.is_closed, {{ var("default_boolean") }}) as is_closed	,
 isnull(stg.is_escalated, {{ var("default_boolean") }}) as is_escalated	,
@@ -53,8 +50,6 @@ isnull(rt.name , '{{ var("default_varchar") }}') as support_type	,
 isnull(stg.solution_c, '{{ var("default_varchar") }}') as solution	,
 isnull(stg.status , '{{ var("default_varchar") }}') as status	,
 isnull(stg.subject , '{{ var("default_varchar") }}') as subject	,
-isnull(stg.survey_send_date_time_c ,'{{ var("default_date") }}') as survey_send_date_time	,
-isnull(stg.thread_id_c , '{{ var("default_varchar") }}') as thread_id	,
 isnull(stg.type, '{{ var("default_varchar") }}') as case_type
 from rawdata as stg
 left outer join  {{ ref("dim_account") }} as a
@@ -71,12 +66,9 @@ select
  ,sfdc_account_id::VARCHAR(300)
  ,case_number::VARCHAR(90)
  ,case_owner_email::VARCHAR(400)
- ,case_ready_to_survey::BOOLEAN
  ,closed_date::TIMESTAMP
- ,confirmed_resolution::BOOLEAN
  ,contact_id::VARCHAR(300)
  ,created_date::TIMESTAMP
- ,csat_response::VARCHAR(1000)
  ,description::VARCHAR(max)
  ,is_closed::BOOLEAN
  ,is_escalated::BOOLEAN
@@ -89,8 +81,6 @@ select
  ,solution::VARCHAR(5000)
  ,status::VARCHAR(100)
  ,subject::VARCHAR(1000)
- ,survey_send_date_time::TIMESTAMP
- ,thread_id::VARCHAR(300)
  ,case_type::VARCHAR(100)
  ,'{{ var("loaddate") }}'::timestamp as loaddate
 from data
